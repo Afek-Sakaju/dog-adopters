c85338c7dfd0ecb1978248063e4fff65535d034c
afekTheMiniLearner <afeksa2000@gmail.com>
2022-08-10 20:12:31 +0300

    controllers added

--- src/routers/dogs.router.js
+++ src/routers/dogs.router.js
@@ -4,84 +4,31 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const path = require('path');
 const {
+    getDogById,
     validateDogBody,
+    createNewDog,
     requiredDogBodyField,
+    deleteDogById,
 } = require('../middleware/validate-dog.middleware');
 const mockPath = path.join(__dirname, '..', 'mocks', 'MOCK_DOGS_DATA.json');
-const { getFilteredDogListCtrl } = require('../controllers/dogs.controller');
+const {
+    getFilteredDogListCtrl,
+    filterDogFromQuery,
+} = require('../controllers/dogs.controller');
 
 const router = express.Router();
 
 //localhost:3000/dogs/123
-router.get('/:dogId', (req, res, next) => {
-    const dog = dogsList.find((dog) => dog.id === req.params.dogId);
-    res.json(dog);
-});
+router.get('/:dogId', getDogById);
 
 //localhost:3000/dogs?status=available&gender=M
 //{status: 'available', gender: 'M'}
-router.get('/', (req, res, next) => {
-    const { status, gender, race, minAge, maxAge, name } = req.query;
-    const dogs = dogsList.filter((dog) => {
-        if (status !== undefined && dog.status !== status) return false;
-        if (
-            gender !== undefined &&
-            dog.gender.toLowerCase() !== gender.toLowerCase()
-        )
-            return false;
-        if (
-            race !== undefined &&
-            !race
-                .split(',')
-                .map((r) => r.trim().toLowerCase())
-                .includes(dog.race.toLowerCase())
-        )
-            return false;
-        if (minAge !== undefined && !(dog.age >= minAge)) return false;
-        if (maxAge !== undefined && !(dog.age <= maxAge)) return false;
-        if (
-            name !== undefined &&
-            !dog.name.toLowerCase().includes(name.toLowerCase())
-        ) {
-            return false;
-        }
-        return true;
-    });
-    res.json(dogs);
-});
+router.get('/', filterDogFromQuery);
 
-router.post('/', validateDogBody, requiredDogBodyField, (req, res, next) => {
-    const dog = {
-        id: uuidv4(),
-        race: req.body.race ?? 'unknown',
-        gender: req.body.gender,
-        age: req.body.age,
-        vaccines: req.body.vaccines ?? 0,
-        behave: req.body.behave ?? [],
-        image: req.body.image,
-        name: req.body.name ?? '',
-        status: 'available',
-    };
-
-    dogsList.push(dog);
-
-    fs.writeFileSync(mockPath, JSON.stringify(dogsList), { encoding: 'utf8' });
-
-    res.sendStatus(201);
-});
+router.post('/', validateDogBody, requiredDogBodyField, createNewDog);
 
 router.put('/:dogId', validateDogBody, getFilteredDogListCtrl);
 
-router.delete('/:dogId', (req, res, next) => {
-    const dogIndex = dogsList.findIndex((d) => d.id === req.params.dogId);
-
-    if (dogIndex === -1) return res.sendStatus(204);
-
-    dogsList.splice(dogIndex, 1);
-
-    fs.writeFileSync(mockPath, JSON.stringify(dogsList), { encoding: 'utf8' });
-
-    res.sendStatus(200);
-});
+router.delete('/:dogId', deleteDogById);
 
 module.exports = router;
