4d248432abcf4a511f86bc0a83f4dbc2239d60bc
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2022-08-10 20:12:31 +0300

    controllers added

--- src/controllers/dogs.controller.js
+++ src/controllers/dogs.controller.js
@@ -1,6 +1,11 @@
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
+module.exports.getDogById = function (req, res, next) {
+    const dog = dogsList.find((dog) => dog.id === req.params.dogId);
+    res.json(dog);
+};
+
 module.exports.getFilteredDogListCtrl = function (req, res, next) {
     const dog = dogsList.find((d) => d.id === req.params.dogId);
     // dog is defined by reference
@@ -22,4 +27,66 @@ module.exports.getFilteredDogListCtrl = function (req, res, next) {
     fs.writeFileSync(mockPath, JSON.stringify(dogsList), { encoding: 'utf8' });
 
     res.sendStatus(206);
+};
+
+module.exports.createNewDog = function (req, res, next) {
+    const dog = {
+        id: uuidv4(),
+        race: req.body.race ?? 'unknown',
+        gender: req.body.gender,
+        age: req.body.age,
+        vaccines: req.body.vaccines ?? 0,
+        behave: req.body.behave ?? [],
+        image: req.body.image,
+        name: req.body.name ?? '',
+        status: 'available',
+    };
+
+    dogsList.push(dog);
+
+    fs.writeFileSync(mockPath, JSON.stringify(dogsList), { encoding: 'utf8' });
+
+    res.sendStatus(201);
+};
+
+module.exports.filterDogFromQuery = function (req, res, next) {
+    const { status, gender, race, minAge, maxAge, name } = req.query;
+    const dogs = dogsList.filter((dog) => {
+        if (status !== undefined && dog.status !== status) return false;
+        if (
+            gender !== undefined &&
+            dog.gender.toLowerCase() !== gender.toLowerCase()
+        )
+            return false;
+        if (
+            race !== undefined &&
+            !race
+                .split(',')
+                .map((r) => r.trim().toLowerCase())
+                .includes(dog.race.toLowerCase())
+        )
+            return false;
+        if (minAge !== undefined && !(dog.age >= minAge)) return false;
+        if (maxAge !== undefined && !(dog.age <= maxAge)) return false;
+        if (
+            name !== undefined &&
+            !dog.name.toLowerCase().includes(name.toLowerCase())
+        ) {
+            return false;
+        }
+        return true;
+    });
+    res.json(dogs);
+};
+
+module.exports.deleteDogById = function (req, res, next) {
+    const dogIndex = dogsList.findIndex((d) => d.id === req.params.dogId);
+
+    if (dogIndex === -1) return res.sendStatus(204);
+
+    dogsList.splice(dogIndex, 1);
+
+    fs.writeFileSync(mockPath, JSON.stringify(dogsList), { encoding: 'utf8' });
+
+    res.sendStatus(200);
 };
