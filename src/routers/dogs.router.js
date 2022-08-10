bc0743c5b1cbafea5655bc76da97930103a5b749
afekTheMiniLearner <afeksa2000@gmail.com>
2022-08-10 20:26:39 +0300

    changes to controller

--- src/routers/dogs.router.js
+++ src/routers/dogs.router.js
@@ -1,27 +1,29 @@
 const express = require('express');
-const { validateDogBody } = require('../middleware/validate-dog.middleware');
 const {
-    getDogById,
-    getFilteredDogListCtrl,
-    filterDogFromQuery,
-    createNewDog,
+    validateDogBody,
     requiredDogBodyField,
-    deleteDogById,
+} = require('../middleware/validate-dog.middleware');
+const {
+    getDogByIdCtrl,
+    filterDogFromQueryCtrl,
+    getFilteredDogListCtrl,
+    createNewDogCtrl,
+    deleteDogByIdCtrl,
 } = require('../controllers/dogs.controller');
 
 const router = express.Router();
 
 //localhost:3000/dogs/123
-router.get('/:dogId', getDogById);
+router.get('/:dogId', getDogByIdCtrl);
 
 //localhost:3000/dogs?status=available&gender=M
 //{status: 'available', gender: 'M'}
-router.get('/', filterDogFromQuery);
+router.get('/', filterDogFromQueryCtrl);
 
-router.post('/', validateDogBody, requiredDogBodyField, createNewDog);
+router.post('/', validateDogBody, requiredDogBodyField, createNewDogCtrl);
 
 router.put('/:dogId', validateDogBody, getFilteredDogListCtrl);
 
-router.delete('/:dogId', deleteDogById);
+router.delete('/:dogId', deleteDogByIdCtrl);
 
 module.exports = router;
