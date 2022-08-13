8ea386bd95300099095172d501a42dd4ebe2c2c0
afekTheMiniLearner <afeksa2000@gmail.com>
2022-08-13 23:18:43 +0300

    initialize ts

--- src/passport-config.ts
+++ src/passport-config.ts
@@ -1,10 +1,19 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const userList = require('./mocks/USERS.mock.json');
+const userList: IUSER[] = require('./mocks/USERS.mock.json');
+interface IUSER {
+    id: string;
+    username: string;
+    password: string;
+    phoneNumber: string;
+    fullName: string;
+}
 
 passport.use(
-    new LocalStrategy((username, password, done) => {
-        const user = userList.find((u) => u.username === username);
+    new LocalStrategy((username: string, password: string, done: Function) => {
+        const user: IUSER | undefined = userList.find(
+            (u) => u.username === username
+        );
         if (!user) return done('user not found', null);
         if (user.password !== password) {
             return done('user not match password', null);
@@ -17,7 +26,7 @@ passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
+passport.deserializeUser((id: string, done: Function) => {
     const user = userList.find((u) => u.id === id);
     if (!user) done('user not found', null);
     else done(null, user);
