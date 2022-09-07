609a95209a7d981703f34e288731060a9285e48c
afekTheMiniLearner <afeksa2000@gmail.com>
2022-09-07 22:29:06 +0300

    finished initializing schema of users and dogs

---
+++ src/models/user.model.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+    {
+        username: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        phoneNumber: { type: String },
+        fullName: { type: String, default: 'Anonymous' },
+    },
+    { timestamps: true }
+);
+
+userSchema.index({ username: 1 });
+
+export const UserModel = mongoose.model('users', userSchema);
