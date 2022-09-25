8b2fc51f735946e357841c2322135c2501d8ce5a
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2022-09-25 12:52:52 +0300

    ...

---
+++ src/aggregations/dog.aggregations.ts
@@ -0,0 +1,152 @@
+import { IDogQuery } from '../interfaces/dog.interface';
+import { DogModel } from '../models/dog.model';
+
+export async function filterDogsAggregation(query: IDogQuery) {
+    const {
+        status,
+        gender,
+        race,
+        minAge,
+        maxAge,
+        name,
+        page,
+        itemsPerPage,
+        sortByStatus,
+        sortByGender,
+        sortByRace,
+        sortByAge,
+        sortByName,
+        sortByLastUpdated,
+    } = query;
+
+    const someSortExists = [
+        sortByStatus,
+        sortByGender,
+        sortByRace,
+        sortByAge,
+        sortByName,
+        sortByLastUpdated,
+    ].some((sortBy) => sortBy !== undefined);
+
+    DogModel.aggregate([
+        {
+            $match: {
+                ...((maxAge !== undefined || minAge !== undefined) && {
+                    age: {
+                        ...(minAge && { $gte: minAge }),
+                        ...(maxAge && { $lte: maxAge }),
+                    },
+                }),
+                ...(name !== undefined && {
+                    name: {
+                        $regex: name,
+                        $options: 'i',
+                    },
+                }),
+                ...(race !== undefined && {
+                    race: { $in: race },
+                }),
+                ...(status !== undefined && {
+                    status: status,
+                }),
+                ...(gender !== undefined && {
+                    gender: gender,
+                }),
+            },
+        },
+        {
+            $lookup: {
+                let: { ownerId: '$owner' },
+                from: 'users',
+                as: 'owner',
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: {
+                                $eq: ['$_id', '$$ownerId'],
+                            },
+                        },
+                    },
+                    {
+                        $project: { username: 1, _id: 0 },
+                    },
+                ],
+            },
+        },
+        { $unwind: '$owner' },
+        {
+            $project: {
+                age: 1,
+                dogName: '$name', // destruction
+                race: 1,
+                gender: 1,
+                owner: '$owner.username',
+                image: 1,
+            },
+        },
+        {
+            $facet: {
+                pagination: [
+                    {
+                        $group: {
+                            _id: null,
+                            totalItems: { $sum: 1 },
+                        },
+                    },
+                    {
+                        $addFields: {
+                            page, // same as page:page
+                            itemsPerPage,
+                            totalPages: {
+                                $divide: ['$totalItems', itemsPerPage],
+                            },
+                        },
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            page: 1,
+                            totalItems: 1,
+                            itemsPerPage: 1,
+                            totalPages: { $ceil: '$totalPages' },
+                        },
+                    },
+                ],
+                data: [
+                    ...((someSortExists
+                        ? [
+                              {
+                                  $sort: {
+                                      ...(sortByAge !== undefined && {
+                                          age: sortByAge,
+                                      }),
+                                      ...(sortByGender !== undefined && {
+                                          gender: sortByGender,
+                                      }),
+                                      ...(sortByName !== undefined && {
+                                          name: sortByName,
+                                      }),
+                                      ...(sortByRace !== undefined && {
+                                          race: sortByRace,
+                                      }),
+                                      ...(sortByStatus !== undefined && {
+                                          status: sortByStatus,
+                                      }),
+                                      ...(sortByLastUpdated !== undefined && {
+                                          updatedAt: sortByLastUpdated,
+                                      }),
+                                  },
+                              },
+                          ]
+                        : []) as any),
+                    {
+                        $skip: (page - 1) * itemsPerPage,
+                    },
+                    {
+                        $limit: itemsPerPage,
+                    },
+                ],
+            },
+        },
+    ]);
+}
