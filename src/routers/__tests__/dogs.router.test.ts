ad461362997599991ea92d88b87a8d3d70d347a7
Hadriel Benjo <hdriel@gmail.com>
2022-09-29 00:21:58 +0300

    add jest supertest tests

---
+++ src/routers/__tests__/dogs.router.test.ts
@@ -0,0 +1,69 @@
+import request from 'supertest';
+import { UserModel } from '../../models';
+import app from '../../app';
+import { IUser } from '../../interfaces/user.interface';
+
+describe('dogs route tests', function () {
+    let user: IUser;
+    let password = 'pass-1234567';
+    let cookie: string;
+
+    beforeAll(async () => {
+        user = (await new UserModel({
+            username: 'dog-auth-user-test',
+            password,
+        }).save()) as unknown as IUser;
+
+        const result = await request(app)
+            .post('/auth/login')
+            .set('Accept', 'application/json')
+            .send({ username: user.username, password });
+
+        cookie = result.headers['set-cookie'][0];
+    });
+    afterAll(() => {
+        UserModel.findByIdAndDelete(user._id);
+    });
+
+    it('responds create dog API - successfully', async function () {
+        const body = {
+            adopter: null,
+            race: 'mixed',
+            gender: 'M',
+            age: 9,
+            vaccines: 4,
+            behave: ['agressive'],
+            name: 'avis',
+        };
+
+        const result = await request(app)
+            .post('/dogs')
+            .set('Accept', 'application/json')
+            .send(body)
+            .set('Cookie', [cookie])
+            .expect(201);
+
+        expect(result).toHaveProperty('_body._id');
+        expect(result).toHaveProperty('_body.name', body.name);
+        expect(result).toHaveProperty('_body.owner', user._id.toString());
+    });
+
+    it('responds create dog API - failure - missing authenticated user cookie', function (done) {
+        const body = {
+            owner: null,
+            adopter: null,
+            race: 'mixed',
+            gender: 'M',
+            age: 9,
+            vaccines: 4,
+            behave: ['agressive'],
+            name: 'avis',
+        };
+
+        request(app)
+            .post('/dogs')
+            .set('Accept', 'application/json')
+            .send(body)
+            .expect(302, done);
+    });
+});
---
+++ src/routers/__tests__/main.router.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest';
+import app from '../../app';
+
+describe('main route tests', function () {
+    it('responds health API with OK status', function (done) {
+        request(app)
+            .get('/health')
+            .expect(200)
+            .end((err, res) => {
+                expect(res).toHaveProperty('text', 'OK');
+                done();
+            });
+    });
+
+    it('responds main API with OK status', function (done) {
+        request(app).get('/health').expect(200, done);
+    });
+});
