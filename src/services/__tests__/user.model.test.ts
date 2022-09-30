dd78115bdab37bbc1618d4d322d8ab0135c38436
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2022-09-30 20:06:44 +0300

    few testing

--- src/services/__tests__/user.model.test.ts
+++ src/services/__tests__/user.model.test.ts
@@ -1,5 +1,22 @@
 import { createNewUser } from '../user.service';
 import { IUser } from '../../interfaces/user.interface';
+import { UserModel } from '../../models/user.model';
+
+describe('bcrypt - hash password tests', () => {
+    test('create a new user should hash his password', async () => {
+        const userTest = {
+            username: 'dog-adopter-user-test',
+            password: 'hahaha',
+        };
+
+        const userDoc = new UserModel(userTest);
+        const res = await userDoc.save();
+
+        expect(res).toHaveProperty('username', userTest.username);
+        expect(res.password).toBeDefined();
+        expect(res.password).not.toBe(userTest.password);
+    });
+});
 
 describe('user model tests', () => {
     test('isAdmin flag not set', async () => {
