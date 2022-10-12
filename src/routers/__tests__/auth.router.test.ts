05f837bb012030432dfc1ff4aba2755013dd6c12
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2022-10-12 14:46:29 +0300

    test cookie login get user not working

--- src/routers/__tests__/auth.router.test.ts
+++ src/routers/__tests__/auth.router.test.ts
@@ -5,23 +5,35 @@ describe('auth route tests', function () {
     const username = 'admin';
     const password = 'admin';
 
-    test('responds login API successfully', function (done) {
-        const body = { username, password };
+    test('responds login API successfully with connect.sid cookie', async function () {
+        {
+            const body = { username, password };
+            var cookie: string | undefined;
 
-        request(app)
-            .post('/auth/login')
-            .set('Accept', 'application/json')
-            .send(body)
-            .expect(302)
-            .end((err, res) => {
-                expect(err).toBe(null);
-                expect(res).toHaveProperty('headers.set-cookie');
-                expect(res.headers['set-cookie']).toHaveLength(1);
-                expect(res.headers['set-cookie'][0]).toMatch(
-                    /connect\.sid=\w*/
-                );
-                done();
-            });
+            await request(app)
+                .post('/auth/login')
+                .set('Accept', 'application/json')
+                .send(body)
+                .expect(302)
+                .end((err, res) => {
+                    expect(err).toBe(null);
+                    expect(res).toHaveProperty('headers.set-cookie');
+                    expect(res.headers['set-cookie']).toHaveLength(1);
+                    expect(res.headers['set-cookie'][0]).toMatch(
+                        /connect\.sid=\w*/
+                    );
+                    debugger;
+                });
+        }
+        {
+            await request(app)
+                .get('/auth/632a1720445b9c30fc10776a')
+                .set('Cookie', [cookie as unknown as string])
+                .end((err, res) => {
+                    expect(res).toHaveProperty('_username', 'admin');
+                    expect(res).toHaveProperty('_password', 'admin');
+                });
+        }
     });
 
     test('login failure incorrect username or password', async function () {
@@ -58,6 +70,16 @@ describe('auth route tests', function () {
         }
         {
             const body = { username };
+
+            await request(app)
+                .post('/auth/login')
+                .set('Accept', 'application/json')
+                .send(body)
+                .expect('Location', '/login.html')
+                .expect(302);
+        }
+        {
+            const body = {};
 
             await request(app)
                 .post('/auth/login')
