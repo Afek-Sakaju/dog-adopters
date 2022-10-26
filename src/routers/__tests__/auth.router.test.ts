f9a02fd4fc46a78b03a7ed58644ade13f4234c1a
afekTheMiniLearner <afeksa2000@gmail.com>
2022-10-26 20:46:16 +0300

    to do added

--- src/routers/__tests__/auth.router.test.ts
+++ src/routers/__tests__/auth.router.test.ts
@@ -23,6 +23,7 @@ describe('auth route tests', function () {
     test('login API success & get connect.sid cookie & login API failed - wrong data', async function () {
         // Keep this test first because the cookie is necessary in other tests
         {
+            //to do move to fucntion get cookie
             const result = await request(app)
                 .post('/auth/login')
                 .set('Accept', 'application/json')
@@ -58,6 +59,8 @@ describe('auth route tests', function () {
 
     test('login API - failure username or password not provided', async function () {
         try {
+            /* catching error is important here because the passport strategy middleware
+            does not continue the operation*/
             const body = { password: userData.password };
 
             await request(app)
@@ -69,6 +72,8 @@ describe('auth route tests', function () {
         } catch (e) {}
 
         try {
+            /* catching error is important here because the passport strategy middleware
+            does not continue the operation*/
             const body = { username: userData.username };
 
             await request(app)
@@ -79,7 +84,7 @@ describe('auth route tests', function () {
                 .expect(302);
         } catch (e) {}
 
-        try {
+        {
             const body = {};
 
             await request(app)
@@ -88,24 +93,32 @@ describe('auth route tests', function () {
                 .send(body)
                 .expect('Location', '/login.html')
                 .expect(302);
-        } catch (e) {}
+        }
     });
 
     test('logout API make the cookie expired check', async function () {
-        try {
-            //fix me
+        {
+            const result = await request(app)
+                .post('/auth/login')
+                .set('Accept', 'application/json')
+                .send(userData)
+                .expect(302);
+
+            const [cookie] = result.headers['set-cookie'];
+            // to do: before each
             debugger;
+            const userID = userDoc._id.toString();
 
             const result2 = await request(app)
-                .get(`/auth/${userDoc?._id.toString()}`)
+                .get(`/auth/${userID}`)
                 .set('Cookie', [cookie])
                 .expect(200);
 
             debugger;
             expect(result2).toBeDefined();
-        } catch (e) {}
+        }
 
-        {
+        /*{
             await request(app)
                 .post('/auth/logout')
                 .set('Cookie', [cookie as string])
@@ -115,20 +128,19 @@ describe('auth route tests', function () {
                 .get(`/auth/${userDoc?._id.toString()}`)
                 .set('Cookie', [cookie as string])
                 .expect(500);
-        }
+        }*/
     });
 
     test('user register API successfully & fail - missing/invalid required data', async () => {
-        try {
+        {
             const body = { username: 'afek123', password: 'afek222' };
 
             const result = await request(app)
                 .post('/auth/register')
                 .set('Accept', 'application/json')
                 .send(body)
-                .expect(200);
+                .expect(201);
 
-            debugger;
             //the test dont come to the debugger
             const generatedHash = bcrypt.hashSync(
                 body.password,
@@ -139,6 +151,6 @@ describe('auth route tests', function () {
 
             expect(result).toHaveProperty('username', body.username);
             expect(isMatch).toBeTruthy();
-        } catch (e) {}
+        }
     });
 });
