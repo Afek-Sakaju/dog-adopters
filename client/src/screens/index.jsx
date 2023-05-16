0f2e188a079762758b1bd693cc33aba97329cf13
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-05-16 16:23:40 +0300

    installed react router dom + initialized the relevant routes + added @screens as abs path

---
+++ client/src/screens/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
+
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+import Profile from "./Profile/Profile";
+import EditDog from "./EditDog/EditDog";
+import AddDog from "./AddDog/AddDog";
+import DogsList from "./DogsList/DogsList";
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/users/:userId" element={<Profile />} />
+                <Route path="/dogs/:dogId" element={<EditDog />} />
+                <Route path="/dogs/new" element={<AddDog />} />{" "}
+                <Route path="/dogs/:query" element={<DogsList />} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
