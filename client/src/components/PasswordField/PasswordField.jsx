9fe8a66892939b5fc94604597c946783a3bfc9e9
afekTheMiniLearner <afeksa2000@gmail.com>
2023-05-20 13:58:02 +0300

    initialized the template of passwordField cmp

---
+++ client/src/components/PasswordField/PasswordField.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { PasswordInput } from './PasswordField.styled';
+
+export default function PasswordField({ ...props }) {
+  return <PasswordInput {...props} />;
+}
