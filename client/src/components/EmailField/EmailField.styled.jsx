eef400e178b30b9821c4d98c9d3862bd029be1ef
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-05-20 13:17:40 +0300

    fixed abs path eslint error and created emailField cmp including stories

---
+++ client/src/components/EmailField/EmailField.styled.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+
+import { TextField } from '@base-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const EmailInput = styled(({ ...props }) => (
+  <TextField type="text" label="Email address" required {...props} />
+))``;
