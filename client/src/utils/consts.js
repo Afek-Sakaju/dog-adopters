0f684928705043c73e720cd63883d0baa57b7301
afekTheMiniLearner <afeksa2000@gmail.com>
2023-05-20 13:30:30 +0300

    finished design of textField

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,8 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
-export const MUI_COlORS = [
-  'primary',
-  'success',
-  'warning',
-  'error',
-  'information',
-];
+export const MUI_COlORS = ['primary', 'success', 'warning', 'error'];
