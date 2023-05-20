4af23f59b8cfbd5403ac9d0b644b95a55d71b4a0
afekTheMiniLearner <afeksa2000@gmail.com>
2023-05-20 13:17:40 +0300

    fixed abs path eslint error and created emailField cmp including stories

---
+++ client/src/components/EmailField/__stories__/EmailField.stories.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+
+import EmailField from '../EmailField';
+
+export default {
+  title: 'components/EmailField',
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          width: '800px',
+          height: '800px',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  component: EmailField,
+};
+
+export const Default = () => <EmailField />;
+
+const Template = (args) => <EmailField {...args} />;
+
+export const Custom = Template.bind({});
+
+Custom.argTypes = {
+  label: { control: { type: 'text' }, defaultValue: 'Email address' },
+  value: { control: { type: 'text' }, defaultValue: '' },
+  variant: {
+    control: 'inline-radio',
+    options: ['filled', 'standard', 'outlined'],
+    defaultValue: 'standard',
+  },
+  color: {
+    control: 'inline-radio',
+    options: ['primary', 'success', 'warning', 'error', 'information'],
+    defaultValue: 'primary',
+  },
+  fullWidth: {
+    control: { type: 'boolean' },
+    defaultValue: false,
+  },
+  required: {
+    control: { type: 'boolean' },
+    defaultValue: false,
+  },
+  Disabled: {
+    control: { type: 'boolean' },
+    defaultValue: false,
+  },
+  type: {
+    control: 'inline-radio',
+    options: ['text', 'search', 'number', 'password'],
+    defaultValue: 'text',
+  },
+  multiline: {
+    control: { type: 'boolean' },
+    defaultValue: false,
+  },
+  rows: {
+    control: { type: 'number', min: 1, max: 5, step: 1 },
+    defaultValue: 1,
+  },
+  maxRows: {
+    control: { type: 'number', min: 1, max: 5, step: 1 },
+    defaultValue: 5,
+  },
+  error: {
+    control: { type: 'boolean' },
+    defaultValue: false,
+  },
+  margin: {
+    control: 'inline-radio',
+    options: ['normal', 'dense'],
+    defaultValue: 'normal',
+  },
+  focused: {
+    control: { type: 'boolean' },
+    defaultValue: false,
+  },
+  helperText: { control: { type: 'text' }, defaultValue: 'Helper-Text' },
+};
+Custom.decorators = [
+  (Story) => (
+    <div
+      style={{
+        padding: '1em',
+        width: '300px',
+        height: '300px',
+      }}
+    >
+      <Story />
+    </div>
+  ),
+];
+
+export const ActiveEmailField = () => {
+  const [text, setText] = useState('');
+
+  return (
+    <EmailField
+      onChange={(event) => {
+        setText(event.target.value);
+      }}
+      value={text}
+    />
+  );
+};
