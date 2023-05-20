0321f6744f90ad5febdd8b8367499d67ef5cc22a
afekTheMiniLearner <afeksa2000@gmail.com>
2023-05-20 13:27:59 +0300

    variant set in default in the story to outlined

--- client/src/components/EmailField/__stories__/EmailField.stories.jsx
+++ client/src/components/EmailField/__stories__/EmailField.stories.jsx
@@ -30,7 +30,7 @@ Custom.argTypes = {
   variant: {
     control: 'inline-radio',
     options: ['filled', 'standard', 'outlined'],
-    defaultValue: 'standard',
+    defaultValue: 'outlined',
   },
   color: {
     control: 'inline-radio',
