9374e3b89a41cbd2eaff5f27a9d0e5d39db63dfc
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-05-21 16:34:47 +0300

    refactored some code related to story and to the architechture of the component

--- client/src/components/EmailField/EmailField.jsx
+++ client/src/components/EmailField/EmailField.jsx
@@ -3,5 +3,13 @@ import React from 'react';
 import { EmailInput } from './EmailField.styled';
 
 export default function EmailField({ ...props }) {
-  return <EmailInput {...props} />;
+  return (
+    <EmailInput
+      variant="outlined"
+      type="text"
+      label="Email address"
+      required
+      {...props}
+    />
+  );
 }
