621e61c561e66b71c982c031b6afe2a7c4361b06
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-05-23 15:38:24 +0300

    variant outlined is by default no need to use it manually at the Fields cmps

--- client/src/components/EmailField/EmailField.jsx
+++ client/src/components/EmailField/EmailField.jsx
@@ -3,13 +3,5 @@ import React from 'react';
 import { EmailInput } from './EmailField.styled';
 
 export default function EmailField({ ...props }) {
-  return (
-    <EmailInput
-      variant="outlined"
-      type="text"
-      label="Email address"
-      required
-      {...props}
-    />
-  );
+  return <EmailInput type="text" label="Email address" required {...props} />;
 }
