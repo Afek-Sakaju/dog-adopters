61b0db08459660b48e817ddeaaf5a0d581f83117
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-05-23 22:38:00 +0300

    now the usage of placements for the label of checkbox used from const MUI_PLACEMENTS

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,2 +1,8 @@
-// eslint-disable-next-line import/prefer-default-export
 export const MUI_COlORS = ['primary', 'success', 'warning', 'error', 'info'];
+
+export const MUI_PLACEMENTS = {
+  start: 'start',
+  bottom: 'bottom',
+  top: 'top',
+  end: 'end',
+};
