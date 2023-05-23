820ed7777e06aa804afd6cf57db57527436a88fb
afekTheMiniLearner <afeksa2000@gmail.com>
2023-05-23 23:29:13 +0300

    moved password and email field cmps to base cmp folder and change all related data including import statements

--- client/src/base-components/EmailField/EmailField.styled.jsx
+++ client/src/base-components/EmailField/EmailField.styled.jsx
@@ -1,6 +1,6 @@
 import { styled } from '@mui/material/styles';
 
-import { TextField } from '@base-components';
+import TextField from '../TextField/TextField';
 
 // eslint-disable-next-line import/prefer-default-export
 export const EmailInput = styled(TextField)``;
