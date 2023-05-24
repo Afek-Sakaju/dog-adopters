c4593f62ea59c8008d3026196507060a3d004e6c
afekTheMiniLearner <afeksacajo@gmail.com>
2023-05-24 14:53:50 +0300

    created button in base components and created its story

---
+++ client/src/base-components/Button/Button.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { MuiButton } from './Button.styled';
+
+export default function Button({
+  label,
+  variant,
+  color,
+  size,
+  disabled,
+  disableElevation,
+  onClick,
+  startIcon,
+  endIcon,
+  ...props
+}) {
+  return (
+    <MuiButton
+      variant={variant}
+      color={color}
+      size={size}
+      disabled={disabled}
+      disableElevation={disableElevation}
+      onClick={onClick}
+      startIcon={startIcon}
+      endIcon={endIcon}
+      {...props}
+    >
+      {label}
+    </MuiButton>
+  );
+}
+
+Button.propTypes = {
+  label: PropTypes.string,
+  variant: PropTypes.oneOf(['text', 'contained', 'outlined']),
+  color: PropTypes.string,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  disabled: PropTypes.bool,
+  disableElevation: PropTypes.bool,
+  onClick: PropTypes.func,
+  startIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  endIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+};
+
+Button.defaultProps = {
+  label: '',
+  variant: 'outlined',
+  color: 'primary',
+  size: 'medium',
+  disabled: undefined,
+  disableElevation: undefined,
+  onClick: undefined,
+  startIcon: undefined,
+  endIcon: undefined,
+};
