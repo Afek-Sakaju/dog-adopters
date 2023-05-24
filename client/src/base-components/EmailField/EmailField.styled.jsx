3b6b08952ad0c2006e87ea1ea76f51f2023036c9
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-05-24 22:11:33 +0300

    removed needless style

--- client/src/base-components/EmailField/EmailField.styled.jsx
+++ client/src/base-components/EmailField/EmailField.styled.jsx
@@ -1,6 +1,4 @@
-import { styled } from '@mui/material/styles';
-
 import TextField from '../TextField/TextField';
 
 // eslint-disable-next-line import/prefer-default-export
-export const EmailInput = styled(TextField)``;
+export const EmailInput = TextField;
