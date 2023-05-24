61afc1c97c431b56babbb32768b5f44b6157f417
afekTheMiniLearner <afeksacajo@gmail.com>
2023-05-24 18:39:45 +0300

    designing the boxes that wrap the app bar content and added some content to its stories

--- client/src/base-components/AppBar/AppBar.jsx
+++ client/src/base-components/AppBar/AppBar.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { MuiAppBar, Toolbar, Typography } from './AppBar.styled';
+import { MuiAppBar, Toolbar, Box, Typography } from './AppBar.styled';
 
-export default function AppBar({ label, position, color, children, ...props }) {
+export default function AppBar({
+  label,
+  position,
+  color,
+  startCmp,
+  children,
+  ...props
+}) {
   return (
     <MuiAppBar position={position} color={color} {...props}>
       <Toolbar>
-        {label ? <Typography variant="h6">{label}</Typography> : null}
-        {children}
+        <Box>
+          {startCmp}
+          {label ? <Typography variant="h6">{label}</Typography> : null}
+        </Box>
+        <Box>{children}</Box>
       </Toolbar>
     </MuiAppBar>
   );
@@ -18,10 +28,12 @@ AppBar.propTypes = {
   label: PropTypes.string,
   position: PropTypes.string,
   color: PropTypes.string,
+  startCmp: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
 };
 
 AppBar.defaultProps = {
   label: undefined,
   position: undefined,
   color: 'primary',
+  startCmp: undefined,
 };
