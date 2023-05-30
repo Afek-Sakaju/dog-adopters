46f0fc44964c665e1110fe935313fc9089179836
afekTheMiniLearner <afeksa2000@gmail.com>
2023-05-31 00:21:49 +0300

    changed the import of the base cmps to come from styled file to allow adding them flex style

--- client/src/screens/Login/Login.styled.jsx
+++ client/src/screens/Login/Login.styled.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
 import MuiPaper from '@mui/material/Paper';
-import MuiBox from '@mui/material/Box';
 import MuiTypography from '@mui/material/Typography';
 import MuiLink from '@mui/material/Link';
 import MuiSnackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
+
+import {
+  TextField as MyTextField,
+  PasswordField as MyPasswordField,
+  Button as MyButton,
+} from '@base-components';
 
 export const Paper = styled(MuiPaper)`
   display: flex;
@@ -13,38 +18,24 @@ export const Paper = styled(MuiPaper)`
   flex-direction: column;
   width: 500px;
   padding: 2em 6em;
-`;
-
-export const ColumnBox = styled(MuiBox)`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  width: 100%;
-  gap: 1.5em;
-  flex: 2;
-`;
-
-export const Box = styled(MuiBox)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: max-content;
-  gap: 0.5em;
+  gap: 5em;
 `;
 
 export const Title = styled(({ children, ...props }) => (
-  <MuiTypography variant="h4" {...props}>
+  <MuiTypography component="div" variant="h4" {...props}>
     {children}
   </MuiTypography>
 ))`
+  flex: 2 1 auto;
   font-weight: bold;
 `;
 
 export const Text = styled(({ children, ...props }) => (
-  <MuiTypography variant="h7" {...props}>
+  <MuiTypography component="div" variant="h7" {...props}>
     {children}
   </MuiTypography>
 ))`
+  flex: 2 1 auto;
   font-weight: bold;
 `;
 
@@ -52,6 +43,34 @@ export const Link = styled(MuiLink)`
   font-weight: bold;
 `;
 
+export const TextField = styled(MyTextField)`
+  flex: 1 2 auto;
+`;
+
+export const PasswordField = styled(MyPasswordField)`
+  flex: 1 2 auto;
+`;
+
+export const Button = styled(MyButton)`
+  flex: 1 2 auto;
+`;
+
 export const Snackbar = MuiSnackbar;
 
 export const Alert = MuiAlert;
+
+// export const ColumnBox = styled(MuiBox)`
+//   display: flex;
+//   align-items: center;
+//   flex-direction: column;
+//   width: 100%;
+//   gap: 1.5em;
+// `;
+
+// export const Box = styled(MuiBox)`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   height: max-content;
+//   gap: 0.5em;
+// `;
