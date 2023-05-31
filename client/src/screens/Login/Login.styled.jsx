85a305e90d8c645678afc8a7ce442f660c797385
Afek-Sakaju <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-05-31 15:31:55 +0300

    transfer the properties of the page to the specific layout of the login instead of in the index.scss file because it can cause unpredictable results in other pages and also removed the old comments in the styled login file

--- client/src/screens/Login/Login.styled.jsx
+++ client/src/screens/Login/Login.styled.jsx
@@ -5,12 +5,21 @@ import MuiTypography from '@mui/material/Typography';
 import MuiLink from '@mui/material/Link';
 import MuiSnackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
+import MuiBox from '@mui/material/Box';
 
 import {
   TextField as MyTextField,
   PasswordField as MyPasswordField,
   Button as MyButton,
 } from '@base-components';
+
+export const PageContainer = styled(MuiBox)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+`;
 
 export const Paper = styled(MuiPaper)`
   display: flex;
@@ -62,19 +71,3 @@ export const Button = styled(MyButton)`
 export const Snackbar = MuiSnackbar;
 
 export const Alert = MuiAlert;
-
-// export const ColumnBox = styled(MuiBox)`
-//   display: flex;
-//   align-items: center;
-//   flex-direction: column;
-//   width: 100%;
-//   gap: 1.5em;
-// `;
-
-// export const Box = styled(MuiBox)`
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   height: max-content;
-//   gap: 0.5em;
-// `;
