61d543a84637b1ef4aa4c49250cf157a9f680e27
Afek-Sakaju <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-04 19:17:17 +0300

    finished register layout

--- client/src/screens/Register/Register.jsx
+++ client/src/screens/Register/Register.jsx
@@ -1,5 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import {
+  Button,
+  PasswordField,
+  TextField,
+  Paper,
+  Title,
+  Text,
+  Link,
+  Alert,
+  Snackbar,
+  PageContainer,
+} from './Register.styled';
 
 export default function Register() {
-    return <div>Register</div>;
+  // Will change those states to Formik usage later
+  const [invalidRegister, setInvalidRegister] = useState(false);
+  // If you want to see the Snackbar alert for visual check, change to true.
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  return (
+    <PageContainer>
+      <Paper variant="elevation" elevation={7}>
+        <Title>Sign Up</Title>
+        <TextField label="Full name" />
+        <TextField label="Phone number" />
+        <TextField label="Username" required />
+        <PasswordField label="Password" />
+        <Text>
+          {'Already have an account ? '}
+          <Link href="http://localhost:3030/login" underline="hover">
+            click here
+          </Link>
+        </Text>
+        <Button label="Register" sx={{ padding: '0.7em' }} fullWidth />
+      </Paper>
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        autoHideDuration={6000}
+        onClose={() => {
+          setIsSubmitted(false);
+          setInvalidRegister(false);
+        }}
+        open={isSubmitted}
+      >
+        {invalidRegister ? (
+          <Alert severity="error">Invalid registration data</Alert>
+        ) : (
+          <Alert severity="success">
+            Registered successfully, you are being redirected...
+          </Alert>
+        )}
+      </Snackbar>
+    </PageContainer>
+  );
 }
