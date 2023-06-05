6747426b857cfcbd4424f5c2f785657b453e7315
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-05 16:48:24 +0300

    changed the userSchema and installed yup

--- client/src/validations/userValidation.js
+++ client/src/validations/userValidation.js
@@ -1,11 +1,9 @@
 import * as yup from 'yup';
 
+// eslint-disable-next-line import/prefer-default-export
 export const userSchema = yup.object().shape({
   username: yup.string().min(5).max(20).required('Please enter username'),
   password: yup.string().min(5).max(20).required('Please enter password'),
-});
-
-export const registerUserSchema = yup.object().shape({
-  username: yup.string().min(5).max(20).required('Please enter username'),
-  password: yup.string().min(5).max(20).required('Please enter password'),
+  fullName: yup.string().min(5).max(20),
+  phoneNumber: yup.string().min(5).max(20),
 });
