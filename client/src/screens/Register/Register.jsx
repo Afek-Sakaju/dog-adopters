4e59292e21e34465e97377b30346760a7e3c9025
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-05 16:44:05 +0300

    created formik for the register form while also split the logic of the register page to be loading form of registration inside the screen from diff file

--- client/src/screens/Register/Register.jsx
+++ client/src/screens/Register/Register.jsx
@@ -1,51 +1,24 @@
 import React, { useState } from 'react';
 
-import {
-  Button,
-  PasswordField,
-  TextField,
-  Paper,
-  Title,
-  Text,
-  Link,
-  Alert,
-  Snackbar,
-  PageContainer,
-} from './Register.styled';
+import { RegisterForm } from '@components';
+import { Alert, Snackbar, PageContainer } from './Register.styled';
 
 export default function Register() {
-  // Will change those states to Formik usage later
-  const [invalidRegister, setInvalidRegister] = useState(false);
-  // If you want to see the Snackbar alert for visual check, change to true.
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [userData, setUserData] = useState({});
+  /* Just sample to know which alert should appear
+  later will be used by actual HTTP code response */
+  const serverResponse = 200;
 
   return (
     <PageContainer>
-      <Paper variant="elevation" elevation={7}>
-        <Title>Sign Up</Title>
-        <TextField label="Full name" />
-        <TextField label="Phone number" />
-        <TextField label="Username" required />
-        <PasswordField label="Password" />
-        <Text>
-          {'Already have an account ? '}
-          <Link href="http://localhost:3030/login" underline="hover">
-            click here
-          </Link>
-        </Text>
-        <Button label="Register" sx={{ padding: '0.7em' }} fullWidth />
-      </Paper>
+      <RegisterForm onSubmit={(data) => setUserData(data)} />
       <Snackbar
         anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
         autoHideDuration={6000}
-        onClose={() => {
-          setIsSubmitted(false);
-          setInvalidRegister(false);
-        }}
-        open={isSubmitted}
+        open={userData?.length}
       >
-        {invalidRegister ? (
-          <Alert severity="error">Invalid registration data</Alert>
+        {serverResponse === 500 ? (
+          <Alert severity="error">Registration failed</Alert>
         ) : (
           <Alert severity="success">
             Registered successfully, you are being redirected...
