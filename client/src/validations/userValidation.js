af0ef8d09c1ea82c1e5d7bdf068ce50aa9aaa645
Afek-Sakaju <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-05 15:57:40 +0300

    modified login validation schema

--- client/src/validations/userValidation.js
+++ client/src/validations/userValidation.js
@@ -2,6 +2,6 @@ import * as yup from 'yup';
 
 // eslint-disable-next-line import/prefer-default-export
 export const userSchema = yup.object().shape({
-  username: yup.string().required('Please enter username'),
+  username: yup.string().min(5).max(20).required('Please enter username'),
   password: yup.string().min(5).max(20).required('Please enter password'),
 });
