1bab84c3f3d933e84b5012b96148e67ec83a3e74
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-05 15:47:13 +0300

    separated the Login page to be page and LoginForm component that loaded in the page

--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -1,73 +1,23 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
-import { withFormik } from 'formik';
-import { userSchema } from '@src/validations';
+import React, { useState } from 'react';
 
-import {
-  Button,
-  PasswordField,
-  TextField,
-  Paper,
-  Title,
-  Text,
-  Link,
-  Alert,
-  Snackbar,
-  PageContainer,
-} from './Login.styled';
+import { LoginForm } from '@components';
+import { Alert, PageContainer, Snackbar } from './Login.styled';
 
-const Login = (props) => {
-  const {
-    errors,
-    handleBlur,
-    handleChange,
-    handleSubmit,
-    isSubmitting,
-    touched,
-    values,
-  } = props;
+export default function Login() {
+  const [userData, setUserData] = useState({});
+  /* Just sample to know which alert should appear
+  later will be used by actual HTTP code response */
+  const serverResponse = 200;
 
+  console.log(userData);
   return (
     <PageContainer>
-      <Paper variant="elevation" elevation={7}>
-        <Title>Sign In</Title>
-        <TextField
-          error={errors.username && touched.username}
-          helperText={touched.username ? errors.username : ''}
-          label="Username"
-          name="username"
-          onBlur={handleBlur}
-          onChange={handleChange}
-          required
-          value={values.username}
-        />
-        <PasswordField
-          error={errors.password && touched.password}
-          helperText={touched.username ? errors.password : ''}
-          label="Password"
-          name="password"
-          onBlur={handleBlur}
-          onChange={handleChange}
-          value={values.password}
-        />
-        <Text>
-          {"Don't have an account yet ? "}
-          <Link href="http://localhost:3030/register" underline="hover">
-            click here
-          </Link>
-        </Text>
-        <Button
-          label="Login"
-          sx={{ padding: '0.7em' }}
-          fullWidth
-          onClick={() => handleSubmit()}
-        />
-      </Paper>
+      <LoginForm onSubmit={(data) => setUserData(data)} />
       <Snackbar
         anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-        open={isSubmitting}
+        open={userData.length}
       >
-        {errors ? (
+        {serverResponse === 500 ? (
           <Alert severity="error">Login failed</Alert>
         ) : (
           <Alert severity="success">Logged in successfully</Alert>
@@ -75,27 +25,4 @@ const Login = (props) => {
       </Snackbar>
     </PageContainer>
   );
-};
-
-export default withFormik({
-  mapPropsToValues: () => ({
-    username: '',
-    password: '',
-    invalidLogin: false,
-    submitAlertShowTime: 2000,
-  }),
-  validationSchema: userSchema,
-
-  handleSubmit: (values, { setSubmitting, resetForm }) => {
-    values.response = 'hey';
-    setTimeout(() => {
-      setSubmitting(false);
-
-      setTimeout(() => {
-        if (values.errors.length) resetForm();
-      }, 100);
-    }, values.submitAlertShowTime);
-  },
-
-  displayName: 'Login',
-})(Login);
+}
