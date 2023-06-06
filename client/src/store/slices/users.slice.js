b43a86b0681ae866757862ef213b86b26f2e6f57
afekTheMiniLearner <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-06 16:12:51 +0300

    added users reducer

---
+++ client/src/store/slices/users.slice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState: {
+    usersList: [],
+    user: {},
+  },
+  reducers: {
+    initUsersList(state, action) {
+      state.usersList = action.payload.usersList;
+    },
+    initUser(state, action) {
+      state.user = action.payload.user;
+    },
+    changeUser(state, action) {
+      const { id } = action.payload;
+      const { user } = state.usersList.find((u) => u.id === id);
+      state.user = user;
+    },
+    removeUser(state) {
+      state.user = {};
+    },
+  },
+});
+
+export const {
+  initUsersList: initUsersListAction,
+  initUser: initUserAction,
+  changeUser: changeUserAction,
+  removeUser: removeUserAction,
+} = usersSlice.actions;
+
+export default usersSlice;
