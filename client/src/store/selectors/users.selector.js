5b0e7505005e8ed2b490ce200af46a5b537fc0bd
afekTheMiniLearner <afeksa2000@gmail.com>
2023-06-09 23:25:38 +0300

    added reselect to improve performance

--- client/src/store/selectors/users.selector.js
+++ client/src/store/selectors/users.selector.js
@@ -1,2 +1,14 @@
-export const getUsersListSelector = (state) => state.users.usersList;
-export const getUserSelector = (state) => state.users.user;
+import { createSelector } from '@reduxjs/toolkit';
+
+const getUsersListSelector = (state) => state.users.usersList;
+const getUserSelector = (state) => state.users.user;
+
+export const getUsersListReselectSelector = createSelector(
+  getUsersListSelector,
+  (usersList) => usersList
+);
+
+export const getUserReselectSelector = createSelector(
+  getUserSelector,
+  (user) => user
+);
