45d931d1967d237be26032516e3bdbcff54a64ad
afekTheMiniLearner <afeksa2000@gmail.com>
2023-06-17 18:33:54 +0300

    added alias for validations folder

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { withFormik } from 'formik';
-import { userSchema } from '@src/validations';
+import { userSchema } from '@validations';
 
 import {
   Button,
