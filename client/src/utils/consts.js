16e9f46d430f7652644e2df9b81a31799536c9a5
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-06-21 18:09:37 +0300

    created DB's urls for the two main project's domains (dogs/auth) and exported their proxy at the consts file, after initializing the proxy file in the utils which include class of proxy for API requests for the server

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,3 +1,5 @@
+import ProxyDB from './proxy';
+
 export const MUI_COLORS = ['primary', 'success', 'warning', 'error', 'info'];
 
 export const MUI_INPUT_TYPES = [
@@ -9,3 +11,11 @@ export const MUI_INPUT_TYPES = [
 ];
 
 export const MUI_PLACEMENTS = ['left', 'right', 'top', 'bottom'];
+
+export const DB_AUTH_URL = 'http://localhost:3000/auth';
+
+export const DB_DOGS_URL = 'http://localhost:3000/dogs';
+
+export const DogsProxy = new ProxyDB(DB_DOGS_URL);
+
+export const AuthProxy = new ProxyDB(DB_AUTH_URL);
---
+++ client/src/utils/proxy.js
@@ -0,0 +1,139 @@
+export default class ProxyDB {
+  constructor(DB_URL) {
+    this.DB_URL = DB_URL;
+  }
+
+  async getData(path = null) {
+    const url = path ? `${this.DB_URL}/${path}` : this.DB_URL;
+    const method = 'GET';
+    let response;
+
+    await fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(`error:${res.status},method:${method},url:${url}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(`${method} Request done successfully`);
+        response = data;
+      })
+      .catch((e) => {
+        console.log(e);
+        response = null;
+      });
+
+    return response;
+  }
+
+  async getDataById(id, path = null) {
+    const url = path ? `${this.DB_URL}/${path}/${id}` : `${this.DB_URL}/${id}`;
+    const method = 'GET';
+    let response;
+
+    await fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(`error:${res.status},method:${method},url:${url}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(`${method} Request done successfully`);
+        response = data;
+      })
+      .catch((e) => {
+        console.log(e);
+        response = null;
+      });
+
+    return response;
+  }
+
+  async post(data, path = null) {
+    const url = path ? `${this.DB_URL}/${path}` : this.DB_URL;
+    const method = 'POST';
+    let response;
+
+    await fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(`error:${res.status},method:${method},url:${url}`);
+        }
+        return res.json();
+      })
+      .then(() => {
+        console.log(`${method} Request done successfully`);
+        response = true;
+      })
+      .catch((e) => {
+        console.log(e);
+        response = false;
+      });
+
+    return response;
+  }
+
+  async put(data, id, path = null) {
+    const url = path ? `${this.DB_URL}/${path}/${id}` : `${this.DB_URL}/${id}`;
+    const method = 'PUT';
+    let response;
+
+    await fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(`error:${res.status},method:${method},url:${url}`);
+        }
+        return res.json();
+      })
+      .then(() => {
+        console.log(`${method} Request done successfully`);
+        response = true;
+      })
+      .catch((e) => {
+        console.log(e);
+        response = false;
+      });
+
+    return response;
+  }
+
+  async delete(id, path = null) {
+    const url = path ? `${this.DB_URL}/${path}/${id}` : `${this.DB_URL}/${id}`;
+    const method = 'DELETE';
+    let response;
+
+    await fetch(url, {
+      method,
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(`error:${res.status},method:${method},url:${url}`);
+        }
+        return res.json();
+      })
+      .then(() => {
+        console.log(`${method} Request done successfully`);
+        response = true;
+      })
+      .catch((e) => {
+        console.log(e);
+        response = false;
+      });
+
+    return response;
+  }
+}
