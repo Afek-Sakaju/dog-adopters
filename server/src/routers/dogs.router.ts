b8663a3337514a703bb187e7a5e90655b51afd90
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-06-25 19:11:43 +0300

    fixed old bug of wrong export value of express in the server files

--- server/src/routers/dogs.router.ts
+++ server/src/routers/dogs.router.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from 'src/express';
+import express, { NextFunction, Request, Response } from 'express';
 import logger from '../utils/logger';
 import uploadDogProfileMW from '../middleware/upload-resource.middleware';
 
