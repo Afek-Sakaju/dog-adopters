dd7e91d3ed01e9018ab57faf4a393e546d370186
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-06-25 17:07:39 +0300

    removed the const MUI_PLACEMENT from consts.js

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,7 +1,5 @@
 import ProxyDB from './proxy';
 
-export const MUI_PLACEMENTS = ['left', 'right', 'top', 'bottom'];
-
 export const DogsProxy = new ProxyDB(process.env.DOGS_URL);
 
 export const AuthProxy = new ProxyDB(process.env.AUTH_URL);
