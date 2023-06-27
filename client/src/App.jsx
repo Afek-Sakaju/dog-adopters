806f150cb550a13fb8b290c7a824b5d4d68e9b9f
afekTheMiniLearner <afeksa2000@gmail.com>
2023-06-27 22:50:18 +0300

    changed folder name to proxies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Router from '@screens';
-import { AuthProxy } from './proxy';
+import { AuthProxy } from './proxies';
 
 export default function App() {
   return <Router />;
