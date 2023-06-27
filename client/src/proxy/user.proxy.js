02d33dff3475b51257cb68e821118d1d23f3418c
Afek-Sakaju <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-27 16:25:41 +0300

    finished auth. proxy

--- client/src/proxy/user.proxy.js
+++ client/src/proxy/user.proxy.js
@@ -1,17 +1,36 @@
-export default class AuthProxy extends Proxy {
-  async getUserByID(id) {
+export default class UsersProxy extends Proxy {
+  async getUserByID({id}) {
     const user = await super.getDataById(id);
     return user;
   }
 
   async getAuthenticatedUserData() {
-    const user = await super.getData('authenticatedUserData');
+		const path = 'authenticatedUserData';
+    const user = await super.getData({path});
     return user;
   }
 
-  async createUser(userData) {
+  async registerUser({userData}) {
     const path = 'register';
-    const user = await super.post(userData, path);
+    const user = await super.post({userData, path});
     return user;
   }
+
+	async loginUser({userData}) {
+    const path = 'login';
+    const user = await super.post({userData, path});
+    return user;
+  }
+
+	async logoutUser() {
+    const path = 'logout';
+    const user = await super.post({path});
+    return user;
+  }
+
+	/* Add the following methods later if need to:
+
+	-	getUsers
+	-	updateUserById
+	-	deleteUserById */
 }
