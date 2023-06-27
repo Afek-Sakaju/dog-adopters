715f14fa11d52c8ccdbce59b04697fd4a72de79c
Afek-Sakaju <100536372+afekTheMiniLearner@users.noreply.github.com>
2023-06-27 17:14:18 +0300

    finished auth. proxy

--- client/src/proxy/index.js
+++ client/src/proxy/index.js
@@ -1,4 +1,5 @@
 import Proxy from './proxy';
+import UserProxy from './user.proxy';
 
 export const DogsProxy = new Proxy(process.env.DOGS_URL);
-export const AuthProxy = new Proxy(process.env.AUTH_URL);
+export const AuthProxy = new UserProxy(process.env.AUTH_URL);
--- client/src/proxy/user.proxy.js
+++ client/src/proxy/user.proxy.js
@@ -1,35 +1,37 @@
-export default class UsersProxy extends Proxy {
-  async getUserByID({id}) {
-    const user = await super.getDataById(id);
+import Proxy from './proxy';
+
+export default class UserProxy extends Proxy {
+  async getUserByID({ id }) {
+    const user = await super.getDataById({ id });
     return user;
   }
 
   async getAuthenticatedUserData() {
-		const path = 'authenticatedUserData';
-    const user = await super.getData({path});
+    const path = 'authenticatedUserData';
+    const user = await super.getData({ path });
     return user;
   }
 
-  async registerUser({userData}) {
+  async registerUser({ userData }) {
     const path = 'register';
-    const user = await super.post({userData, path});
+    const user = await super.post({ userData, path });
     return user;
   }
 
-	async loginUser({userData}) {
+  async loginUser({ userData }) {
     const path = 'login';
-    const user = await super.post({userData, path});
+    const user = await super.post({ userData, path });
     return user;
   }
 
-	async logoutUser() {
+  async logoutUser() {
     const path = 'logout';
-    const user = await super.post({path});
+    const user = await super.post({ path });
     return user;
   }
 
-	/* Add the following methods later if need to:
-
+  /* Add the following methods later if need to:
+	
 	-	getUsers
 	-	updateUserById
 	-	deleteUserById */
