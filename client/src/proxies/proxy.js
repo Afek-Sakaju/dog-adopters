da498e48eeaf05fbbf84f753ae85a968d52afde3
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-01 22:30:50 +0300

    changed from fetch to axios

--- client/src/proxies/proxy.js
+++ client/src/proxies/proxy.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default class BaseProxy {
   constructor(url) {
     this.url = url;
@@ -6,123 +8,75 @@ export default class BaseProxy {
   async getData(path = '') {
     const requestUrl = `${this.url}/${path}`;
     const method = 'GET';
-    let response;
 
-    await fetch(requestUrl)
-      .then((res) => {
-        if (!res.ok) {
-          throw Error(`error:${res.status},method:${method},url:${requestUrl}`);
-        }
-        return res.json();
-      })
-      .then((data) => {
-        response = data;
-      })
-      .catch((e) => {
-        console.error(e);
-        response = '';
-      });
-
-    return response;
+    try {
+      const response = await axios.get(requestUrl);
+      return response.data;
+    } catch (error) {
+      console.error(
+        `error:${error.response.status},method:${method},url:${requestUrl}`
+      );
+      return '';
+    }
   }
 
   async getDataById(id, path = '') {
     const requestUrl = path ? `${this.url}/${path}/${id}` : `${this.url}/${id}`;
     const method = 'GET';
-    let response;
 
-    await fetch(requestUrl)
-      .then((res) => {
-        if (!res.ok) {
-          throw Error(`error:${res.status},method:${method},url:${requestUrl}`);
-        }
-        return res.json();
-      })
-      .then((data) => {
-        response = data;
-      })
-      .catch((e) => {
-        console.error(e);
-        response = '';
-      });
-
-    return response;
+    try {
+      const response = await axios.get(requestUrl);
+      return response.data;
+    } catch (error) {
+      console.error(
+        `error:${error.response.status},method:${method},url:${requestUrl}`
+      );
+      return '';
+    }
   }
 
   async post(data, path = '') {
     const requestUrl = `${this.url}/${path}`;
     const method = 'POST';
-    let response;
 
-    await fetch(requestUrl, {
-      method,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw Error(`error:${res.status},method:${method},url:${requestUrl}`);
-        }
-        return res.json();
-      })
-      .then(() => {
-        response = true;
-      })
-      .catch((e) => {
-        console.error(e);
-        response = false;
-      });
-
-    return response;
+    try {
+      const response = await axios.post(requestUrl, data);
+      return response.data;
+    } catch (error) {
+      console.error(
+        `error:${error.response.status},method:${method},url:${requestUrl}`
+      );
+      return false;
+    }
   }
 
   async put(data, id, path = '') {
     const requestUrl = path ? `${this.url}/${path}/${id}` : `${this.url}/${id}`;
     const method = 'PUT';
-    let response;
 
-    await fetch(requestUrl, {
-      method,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw Error(`error:${res.status},method:${method},url:${requestUrl}`);
-        }
-        return res.json();
-      })
-      .then(() => {
-        response = true;
-      })
-      .catch((e) => {
-        console.error(e);
-        response = false;
-      });
-
-    return response;
+    try {
+      await axios.put(requestUrl, data);
+      return true;
+    } catch (error) {
+      console.error(
+        `error:${error.response.status},method:${method},url:${requestUrl}`
+      );
+      return false;
+    }
   }
 
   async delete(id, path = '') {
     const requestUrl = path ? `${this.url}/${path}/${id}` : `${this.url}/${id}`;
     const method = 'DELETE';
-    let response;
 
-    await fetch(requestUrl, { method })
-      .then((res) => {
-        if (!res.ok) {
-          throw Error(`error:${res.status},method:${method},url:${requestUrl}`);
-        }
-        return res.json();
-      })
-      .then(() => {
-        response = true;
-      })
-      .catch((e) => {
-        console.error(e);
-        response = false;
-      });
-
-    return response;
+    try {
+      await axios.delete(requestUrl);
+      return true;
+    } catch (error) {
+      console.error(
+        `error:${error.response.status},method:${method},url:${requestUrl}`
+      );
+      return false;
+    }
   }
 }
