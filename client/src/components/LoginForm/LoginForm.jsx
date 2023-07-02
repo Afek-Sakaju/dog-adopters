c15baa05c8f84768dac9622dfab581dbfa28b864
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-03 00:07:55 +0300

    login form now connected to the login request

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { withFormik } from 'formik';
-import { userSchema } from '@validations';
 
+import { userSchema } from '@validations';
+import { AuthProxy } from '@proxies';
 import {
   Button,
   PasswordField,
@@ -14,8 +15,16 @@ import {
 } from './LoginForm.styled';
 
 const LoginForm = (props) => {
-  const { errors, handleBlur, handleChange, handleSubmit, touched, values } =
-    props;
+  const {
+    errors,
+    handleBlur,
+    handleChange,
+    // eslint-disable-next-line no-unused-vars
+    setIsLoggedIn,
+    handleSubmit,
+    touched,
+    values,
+  } = props;
 
   return (
     <Paper variant="elevation" elevation={7}>
@@ -65,6 +74,13 @@ export default withFormik({
   handleSubmit: (values, { props, resetForm }) => {
     const { username, password } = values;
     const data = { username, password };
+
+    AuthProxy.loginUser(data)
+      .then((res) => {
+        const { status } = res;
+        if (status >= 200 && status < 400) props.setIsLoggedIn(true);
+      })
+      .catch((e) => console.log(e));
 
     props.onSubmit?.(data);
     resetForm();
