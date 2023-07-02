d73a9e69d95b4cab3e9d944ce64c488dffbda9b0
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-07-02 18:23:14 +0300

    added login proxy method to the login page not finished yet

--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -1,21 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { LoginForm } from '@components';
+import { AuthProxy } from '@proxies';
 import { Alert, PageContainer, Snackbar } from './Login.styled';
 
 export default function Login() {
   const [userData, setUserData] = useState(null);
-  // Just for demonstration, will be used with API request result
-  const responseCode = 500;
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const res = AuthProxy.loginUser(userData)
+      .then((responseData) => responseData)
+      .catch((e) => console.log(e));
+    setResponse(res);
+  }, [userData]);
 
   return (
     <PageContainer>
       <LoginForm onSubmit={(data) => setUserData(data)} />
       <Snackbar
         anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-        open={responseCode}
+        open={response}
       >
-        {responseCode === 500 ? (
+        {response === 500 ? (
           <Alert severity="error">Login failed</Alert>
         ) : (
           <Alert severity="success">Logged in successfully</Alert>
