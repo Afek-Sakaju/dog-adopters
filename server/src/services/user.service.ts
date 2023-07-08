0ae2e19ea00ebd8de646431e7f361253e4b44f3e
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-07-09 00:22:31 +0300

    changed interface and user model configurations to fix the user service ts and eslint errors

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,4 +1,4 @@
-import { IUser } from '../interfaces/user.interface';
+import { IUser, IUserDoc } from '../interfaces/user.interface';
 import { UserModel } from '../models';
 import { TEST_REQ_ID } from '../test-utils/environment-variables';
 import logger from '../utils/logger';
@@ -6,38 +6,39 @@ import logger from '../utils/logger';
 export async function getUserById(
     userId: string,
     requestId: string = TEST_REQ_ID
-): Promise<IUser | undefined> {
+): Promise<IUserDoc> {
     logger.verbose(requestId, 'Running request to get user by id from DB');
 
-    const userDoc: any = await UserModel.findById(userId);
-    return userDoc?.toJSON() as unknown as IUser | undefined;
+    const userDoc: IUserDoc | null | undefined = await UserModel.findById(
+        userId
+    );
+    return userDoc;
 }
 
 export async function createNewUser(
     user: IUser,
     requestId: string = TEST_REQ_ID
-): Promise<IUser | undefined> {
+): Promise<IUserDoc> {
     logger.verbose(requestId, 'Creation request of new user to DB');
 
     const userDoc = new UserModel(user);
-    const res: any = await userDoc.save();
-
-    return res.toJSON();
+    const res: IUserDoc = await userDoc.save();
+    return res;
 }
 
 export async function getUserByUsername(
     name: string,
     requestId: string = TEST_REQ_ID
-): Promise<IUser | undefined> {
+): Promise<IUserDoc> {
     logger.verbose(
         requestId,
         "Getting user's username and password by his username from DB",
         { username: name }
     );
 
-    const userDoc: any = await UserModel.findOne({ username: name }).select(
-        'username password'
-    );
+    const userDoc: IUserDoc = await UserModel.findOne({
+        username: name,
+    }).select('username password');
 
-    return userDoc as unknown as IUser | undefined;
+    return userDoc;
 }
