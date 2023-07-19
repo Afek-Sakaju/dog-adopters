5cba6f37be4f34da46f6147c0d4c94815f6fe183
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-19 23:04:37 +0300

    now the function removes spaces

--- client/src/utils/functions.js
+++ client/src/utils/functions.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/prefer-default-export
 export const removeSpacesFromObjectValues = (obj) => {
     return Object.keys(obj).reduce((acc, key) => {
-        acc[key] = obj[key]?.replace(' ', '');
+        acc[key] = obj[key]?.split(' ').join('');
         return acc;
     }, {});
 };
