81132dd052ed9bdf50e842c66fd9ff6fcfebf4e7
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-07-20 00:14:29 +0300

    added specific test to allow spaces in the fullname but only between names

--- client/src/validations/userValidation.js
+++ client/src/validations/userValidation.js
@@ -1,5 +1,7 @@
 import * as yup from 'yup';
 
+import { assertSpacesValidity } from '@utils';
+
 const noSpacesRegExp = /^\S*$/;
 const phoneRegExp =
     /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
@@ -22,7 +24,11 @@ export const userSchema = yup.object().shape({
         .string()
         .min(5)
         .max(20)
-        .matches(noSpacesRegExp, 'Full name should not contain spaces'),
+        .test(
+            'assert-spaces-validity',
+            'Full name can contain space only between names',
+            assertSpacesValidity
+        ),
     phoneNumber: yup
         .string()
         .matches(phoneRegExp, 'Please enter a valid phone number')
