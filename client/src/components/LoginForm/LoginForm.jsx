c7d4d69277bf52eeb586f670667bbf5e4b363690
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-07-19 23:05:09 +0300

    using removeSpacesFromObjectValues in the values of both register and login forms to remove spaces from input

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -4,79 +4,85 @@ import { withFormik } from 'formik';
 
 import { userSchema } from '@validations';
 import { AuthProxy } from '@proxies';
+import { removeSpacesFromObjectValues } from '@utils';
 import {
-  Button,
-  PasswordField,
-  TextField,
-  Paper,
-  Title,
-  Text,
-  Link,
+    Button,
+    PasswordField,
+    TextField,
+    Paper,
+    Title,
+    Text,
+    Link,
 } from './LoginForm.styled';
 
 const LoginForm = (props) => {
-  const { errors, handleBlur, handleChange, handleSubmit, touched, values } =
-    props;
+    const { errors, handleBlur, handleChange, handleSubmit, touched, values } =
+        props;
 
-  return (
-    <Paper variant="elevation" elevation={7}>
-      <Title>Sign In</Title>
-      <TextField
-        error={errors.username && touched.username}
-        helperText={touched.username ? errors.username : ''}
-        label="Username"
-        name="username"
-        onBlur={handleBlur}
-        onChange={handleChange}
-        required
-        value={values.username}
-      />
-      <PasswordField
-        error={errors.password && touched.password}
-        helperText={touched.password ? errors.password : ''}
-        label="Password"
-        name="password"
-        onBlur={handleBlur}
-        onChange={handleChange}
-        value={values.password}
-      />
-      <Text>
-        {"Don't have an account yet ? "}
-        <Link href="/register" underline="hover">
-          click here
-        </Link>
-      </Text>
-      <Button
-        label="Login"
-        sx={{ padding: '0.7em' }}
-        fullWidth
-        onClick={() => handleSubmit()}
-      />
-    </Paper>
-  );
+    return (
+        <Paper variant="elevation" elevation={7}>
+            <Title>Sign In</Title>
+            <TextField
+                error={errors.username && touched.username}
+                helperText={touched.username ? errors.username : ''}
+                label="Username"
+                name="username"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                required
+                value={values.username}
+            />
+            <PasswordField
+                error={errors.password && touched.password}
+                helperText={touched.password ? errors.password : ''}
+                label="Password"
+                name="password"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.password}
+            />
+            <Text>
+                {"Don't have an account yet ? "}
+                <Link href="/register" underline="hover">
+                    click here
+                </Link>
+            </Text>
+            <Button
+                label="Login"
+                sx={{ padding: '0.7em' }}
+                fullWidth
+                onClick={() => handleSubmit()}
+            />
+        </Paper>
+    );
 };
 
 export default withFormik({
-  mapPropsToValues: () => ({
-    username: '',
-    password: '',
-  }),
-  validationSchema: userSchema,
+    mapPropsToValues: () => ({
+        username: '',
+        password: '',
+    }),
+    validationSchema: userSchema,
 
-  handleSubmit: async (values, { props, resetForm }) => {
-    await AuthProxy.loginUser(values)
-      .then(() => props.setResponseState?.(1))
-      .then(() => {
-        props.onSubmit?.(values);
-        resetForm();
-      })
-      .catch((e) => {
-        props.onSubmit?.(null);
-        props.setResponseState?.(-1);
-        values.password = '';
-        console.error(e);
-      });
-  },
+    handleSubmit: async (values, { props, resetForm }) => {
+        const { username, password } = values;
+        const loginData = removeSpacesFromObjectValues({ username, password });
 
-  displayName: 'LoginForm',
+        console.log(loginData);
+
+        await AuthProxy.loginUser(loginData)
+            .then(() => props.setResponseState?.(1))
+            .then(() => {
+                props.onSubmit?.(loginData);
+                resetForm();
+            })
+            .catch((e) => {
+                props.onSubmit?.(null);
+                props.setResponseState?.(-1);
+                loginData.password = '';
+                console.error(e);
+            });
+    },
+
+    displayName: 'LoginForm',
 })(LoginForm);
