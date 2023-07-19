0cbe11184d569b996da0319108d72323f9175d06
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-19 23:37:00 +0300

    added regex to make sure there are no spaces in the form. validating that before submitting form removes the necessity of the validation in the submit handler and for the removeSpaces util funciton

--- client/src/utils/functions.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const removeSpacesFromObjectValues = (obj) => {
-    return Object.keys(obj).reduce((acc, key) => {
-        acc[key] = obj[key]?.split(' ').join('');
-        return acc;
-    }, {});
-};
--- client/src/validations/userValidation.js
+++ client/src/validations/userValidation.js
@@ -1,16 +1,32 @@
 import * as yup from 'yup';
 
+const noSpacesRegExp = /^\S*$/;
 const phoneRegExp =
-  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+    /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
 
 // eslint-disable-next-line import/prefer-default-export
 export const userSchema = yup.object().shape({
-  username: yup.string().min(5).max(20).required('Please enter username'),
-  password: yup.string().min(5).max(20).required('Please enter password'),
-  fullName: yup.string().min(5).max(20),
-  phoneNumber: yup
-    .string()
-    .matches(phoneRegExp, 'Please enter valid phone number')
-    .min(7)
-    .max(17),
+    username: yup
+        .string()
+        .min(5)
+        .max(20)
+        .required('Please enter username')
+        .matches(noSpacesRegExp, 'Username should not contain spaces'),
+    password: yup
+        .string()
+        .min(5)
+        .max(20)
+        .required('Please enter password')
+        .matches(noSpacesRegExp, 'Password should not contain spaces'),
+    fullName: yup
+        .string()
+        .min(5)
+        .max(20)
+        .matches(noSpacesRegExp, 'Full name should not contain spaces'),
+    phoneNumber: yup
+        .string()
+        .matches(phoneRegExp, 'Please enter a valid phone number')
+        .min(7)
+        .max(17)
+        .matches(noSpacesRegExp, 'Phone number should not contain spaces'),
 });
