a937b46208d604075718bb74f1cbe9df4cb99837
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-22 16:28:49 +0300

    added dogs proxy and its necessary methods

--- client/src/proxies/index.js
+++ client/src/proxies/index.js
@@ -1,7 +1,6 @@
-// import BaseProxy from './proxy';
 import UserProxy from './user.proxy';
+import DogDataProxy from './dogs.proxy';
 
-// eslint-disable-next-line import/prefer-default-export
 export const AuthProxy = new UserProxy(process.env.AUTH_URL);
 
-// export const DogsProxy = new Proxy(process.env.DOGS_URL);
+export const DogProxy = new DogDataProxy(process.env.DOGS_URL);
--- client/src/proxies/proxy.js
+++ client/src/proxies/proxy.js
@@ -1,78 +1,90 @@
 import axios from 'axios';
 
 export default class BaseProxy {
-  constructor(url) {
-    this.url = url;
-  }
-
-  async getData(path = '') {
-    const requestUrl = `${this.url}/${path}`;
-    const method = 'GET';
-
-    try {
-      const response = await axios.get(requestUrl, { withCredentials: true });
-      return response?.data;
-    } catch (error) {
-      const errorMessage = `error:${error},method:${method},status:${error.response.status},url:${error.response.url}`;
-      throw Error(errorMessage);
+    constructor(url) {
+        this.url = url;
     }
-  }
 
-  async getDataById(id, path = '') {
-    const requestUrl = path ? `${this.url}/${path}/${id}` : `${this.url}/${id}`;
-    const method = 'GET';
+    async getData(path = '') {
+        const requestUrl = `${this.url}/${path}`;
+        const method = 'GET';
 
-    try {
-      const response = await axios.get(requestUrl, { withCredentials: true });
-      return response?.data;
-    } catch (error) {
-      const errorMessage = `error:${error},method:${method},status:${error.response.status},url:${error.response.url}`;
-      throw Error(errorMessage);
+        try {
+            const response = await axios.get(requestUrl, {
+                withCredentials: true,
+            });
+            return response?.data;
+        } catch (error) {
+            const errorMessage = `error:${error},method:${method},status:${error.response.status},url:${error.response.url}`;
+            throw Error(errorMessage);
+        }
     }
-  }
 
-  async post(data, path = '') {
-    const requestUrl = `${this.url}/${path}`;
-    const method = 'POST';
+    async getDataById(id, path = '') {
+        const requestUrl = path
+            ? `${this.url}/${path}/${id}`
+            : `${this.url}/${id}`;
+        const method = 'GET';
 
-    try {
-      const response = await axios.post(requestUrl, data, {
-        withCredentials: true,
-      });
-      return response?.data;
-    } catch (error) {
-      const errorMessage = `error:${error},method:${method},status:${error.response.status},url:${error.response.url}`;
-      throw Error(errorMessage);
+        try {
+            const response = await axios.get(requestUrl, {
+                withCredentials: true,
+            });
+            return response?.data;
+        } catch (error) {
+            const errorMessage = `error:${error},method:${method},status:${error.response.status},url:${error.response.url}`;
+            throw Error(errorMessage);
+        }
     }
-  }
 
-  async put(data, id, path = '') {
-    const requestUrl = path ? `${this.url}/${path}/${id}` : `${this.url}/${id}`;
-    const method = 'PUT';
+    async post(data, path = '', id = '') {
+        const requestUrl = id
+            ? `${this.url}/${path}/${id}`
+            : `${this.url}/${path}`;
+        const method = 'POST';
 
-    try {
-      const response = await axios.put(requestUrl, data, {
-        withCredentials: true,
-      });
-      return response?.data;
-    } catch (error) {
-      const errorMessage = `error:${error},method:${method},status:${error.response.status},url:${error.response.url}`;
-      throw Error(errorMessage);
+        try {
+            const response = await axios.post(requestUrl, data, {
+                withCredentials: true,
+            });
+            return response?.data;
+        } catch (error) {
+            const errorMessage = `error:${error},method:${method},status:${error.response.status},url:${error.response.url}`;
+            throw Error(errorMessage);
+        }
     }
-  }
 
-  async delete(id, path = '') {
-    const requestUrl = path ? `${this.url}/${path}/${id}` : `${this.url}/${id}`;
-    const method = 'DELETE';
+    async put(data, id, path = '') {
+        const requestUrl = path
+            ? `${this.url}/${path}/${id}`
+            : `${this.url}/${id}`;
+        const method = 'PUT';
 
-    try {
-      const response = await axios.delete(requestUrl, {
-        withCredentials: true,
-      });
-      return response?.data;
-    } catch (error) {
-      const errorMessage = `error:${error},method:${method},status:${error.response.status},url:${error.response.url}`;
-      throw Error(errorMessage);
+        try {
+            const response = await axios.put(requestUrl, data, {
+                withCredentials: true,
+            });
+            return response?.data;
+        } catch (error) {
+            const errorMessage = `error:${error},method:${method},status:${error.response.status},url:${error.response.url}`;
+            throw Error(errorMessage);
+        }
     }
-  }
+
+    async delete(id, path = '') {
+        const requestUrl = path
+            ? `${this.url}/${path}/${id}`
+            : `${this.url}/${id}`;
+        const method = 'DELETE';
+
+        try {
+            const response = await axios.delete(requestUrl, {
+                withCredentials: true,
+            });
+            return response?.data;
+        } catch (error) {
+            const errorMessage = `error:${error},method:${method},status:${error.response.status},url:${error.response.url}`;
+            throw Error(errorMessage);
+        }
+    }
 }
