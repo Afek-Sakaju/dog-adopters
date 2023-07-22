982d9e65fc810cab671ff115eb314e2ae7e2ad6e
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-22 19:46:45 +0300

    removed the DogProxy from window

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
 import Router from '@screens';
-import { DogProxy } from '@proxies';
-
-window.DogProxy = DogProxy;
 
 export default function App() {
     return <Router />;
