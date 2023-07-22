8f4243a371ba57a8654362c2dc0aa44f001fe96f
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-07-22 16:32:24 +0300

    change UserProxy to the name of: UserDataProxy Since the dog proxy is called : DogDataProxy i wanted to conserve uniformity of their naming convention

--- client/src/proxies/index.js
+++ client/src/proxies/index.js
@@ -1,6 +1,6 @@
-import UserProxy from './user.proxy';
+import UserDataProxy from './user.proxy';
 import DogDataProxy from './dogs.proxy';
 
-export const AuthProxy = new UserProxy(process.env.AUTH_URL);
+export const AuthProxy = new UserDataProxy(process.env.AUTH_URL);
 
 export const DogProxy = new DogDataProxy(process.env.DOGS_URL);
