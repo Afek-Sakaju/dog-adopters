3ff44de28864e8baa2fb914ce0d48aa82657ad72
afekTheMiniLearner <afeksa2000@gmail.com>
2023-07-23 13:58:53 +0300

    added dog creation page screen route

--- client/src/screens/index.jsx
+++ client/src/screens/index.jsx
@@ -5,6 +5,7 @@ import Login from './Login/Login';
 import Register from './Register/Register';
 import Profile from './Profile/Profile';
 import EditDog from './EditDog/EditDog';
+import CreateDog from './CreateDog/CreateDog';
 import DogsList from './DogsList/DogsList';
 
 export default function Router() {
@@ -14,6 +15,7 @@ export default function Router() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/users/:userId" element={<Profile />} />
+                <Route path="/dogs/new" element={<CreateDog />} />
                 <Route path="/dogs/:dogId" element={<EditDog />} />
                 <Route path="/dogs" element={<DogsList />} />
             </Switch>
