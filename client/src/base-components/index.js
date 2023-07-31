24a2a0e21a983cd3f7ae6f71ceca399495b447af
afekTheMiniLearner <afeksa2000@gmail.com>
2023-08-01 00:05:58 +0300

    Added the usage of Autocomplete mui component for the behave input in the createDogForm

--- client/src/base-components/index.js
+++ client/src/base-components/index.js
@@ -1,5 +1,6 @@
-export { default as Checkbox } from './Checkbox/Checkbox';
-export { default as TextField } from './TextField/TextField';
-export { default as PasswordField } from './PasswordField/PasswordField';
+export { default as Autocomplete } from './Autocomplete/Autocomplete';
 export { default as Button } from './Button/Button';
-export { default as Drawer } from './Button/Button';
+export { default as Checkbox } from './Checkbox/Checkbox';
+export { default as Drawer } from './Drawer/Drawer';
+export { default as PasswordField } from './PasswordField/PasswordField';
+export { default as TextField } from './TextField/TextField';
