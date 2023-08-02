bf5f593a249954f7846601f4f4747d2c00754b72
Afek-Sakaju-BetrBeta <a.sakaju_519@contactbetrbeta.onmicrosoft.com>
2023-08-02 23:40:10 +0300

    removed unnecessary styling

--- client/src/base-components/Autocomplete/Autocomplete.styled.jsx
+++ client/src/base-components/Autocomplete/Autocomplete.styled.jsx
@@ -5,6 +5,4 @@ import MyAutocomplete from '@mui/material/Autocomplete';
 export const MuiAutocomplete = styled(MyAutocomplete)`
     height: 100px;
     max-height: 100px;
-    border: 2px solid red;
-    box-sizing: border-box;
 `;
