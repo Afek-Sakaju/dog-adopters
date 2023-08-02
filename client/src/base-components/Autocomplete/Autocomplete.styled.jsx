1aeb25233e848a0f67c2ef2ba9763a3717f98f23
Afek-Sakaju-BetrBeta <a.sakaju_519@contactbetrbeta.onmicrosoft.com>
2023-08-02 23:32:12 +0300

    Added autocomplete cmp and used an options for behave in a constants file

--- client/src/base-components/Autocomplete/Autocomplete.styled.jsx
+++ client/src/base-components/Autocomplete/Autocomplete.styled.jsx
@@ -2,4 +2,9 @@ import { styled } from '@mui/material/styles';
 import MyAutocomplete from '@mui/material/Autocomplete';
 
 // eslint-disable-next-line import/prefer-default-export
-export const MuiAutocomplete = styled(MyAutocomplete)``;
+export const MuiAutocomplete = styled(MyAutocomplete)`
+    height: 100px;
+    max-height: 100px;
+    border: 2px solid red;
+    box-sizing: border-box;
+`;
