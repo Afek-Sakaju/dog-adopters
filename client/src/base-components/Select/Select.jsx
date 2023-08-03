c3b58f2438e3c9fdbcfafbbff60244c225f1cbef
Afek-Sakaju <afeksacajo@email.com>
2023-08-04 00:06:35 +0300

    Finished implementing proper label for the gender select

--- client/src/base-components/Select/Select.jsx
+++ client/src/base-components/Select/Select.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { MuiSelect, MenuItem } from './Select.styled';
+import { MuiSelect, MenuItem, FormControl, InputLabel } from './Select.styled';
 
 export default function Select({
     currentValue,
     onChange,
     optionsProperties,
     size,
+    title,
+    fullWidth,
     ...props
 }) {
     return (
-        <MuiSelect
-            onChange={(e) => onChange(e.target.value)}
-            size={size}
-            value={currentValue}
-            {...props}
-        >
-            {Object.entries(optionsProperties)?.map(
-                ([label, properties], i) => {
-                    const { style } = properties;
-                    return (
-                        <MenuItem key={i} value={label} sx={style}>
-                            {label}
-                        </MenuItem>
-                    );
-                }
-            )}
-        </MuiSelect>
+        <FormControl fullWidth={fullWidth} variant="filled">
+            <InputLabel shrink>{title}</InputLabel>
+            <MuiSelect
+                onChange={(e) => onChange(e.target.value)}
+                size={size}
+                value={currentValue}
+                {...props}
+            >
+                {Object.entries(optionsProperties)?.map(
+                    ([label, properties], i) => {
+                        const { style } = properties;
+                        return (
+                            <MenuItem key={i} value={label} sx={style}>
+                                {label}
+                            </MenuItem>
+                        );
+                    }
+                )}
+            </MuiSelect>
+        </FormControl>
     );
 }
 
 Select.propTypes = {
     currentValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    fullWidth: PropTypes.bool,
     onChange: PropTypes.func,
     optionsProperties: PropTypes.objectOf(
         PropTypes.shape({
@@ -42,11 +48,14 @@ Select.propTypes = {
         })
     ),
     size: PropTypes.string,
+    title: PropTypes.string,
 };
 
 Select.defaultProps = {
     currentValue: undefined,
+    fullWidth: undefined,
     onChange: undefined,
     optionsProperties: [],
     size: 'small',
+    title: undefined,
 };
--- client/src/base-components/Select/Select.styled.jsx
+++ client/src/base-components/Select/Select.styled.jsx
@@ -1,6 +1,8 @@
 import { styled } from '@mui/material/styles';
 import MySelect from '@mui/material/Select';
 import MuiMenuItem from '@mui/material/MenuItem';
+import MuiFormControl from '@mui/material/FormControl';
+import MuiInputLabel from '@mui/material/InputLabel';
 
 export const MuiSelect = styled(MySelect)`
     height: 56px;
@@ -9,3 +11,7 @@ export const MuiSelect = styled(MySelect)`
 export const MenuItem = styled(MuiMenuItem)`
     font-weight: bolder;
 `;
+
+export const FormControl = MuiFormControl;
+
+export const InputLabel = MuiInputLabel;
