6f2f13bddee92ad5851fcbe122371ee1aece006b
Afek-Sakaju <afeksacajo@email.com>
2023-08-03 23:53:41 +0300

    Added select cmp to the base cmp folder and used it in the Gender input and also fixed some logic and style  issues

---
+++ client/src/base-components/Select/Select.styled.jsx
@@ -0,0 +1,11 @@
+import { styled } from '@mui/material/styles';
+import MySelect from '@mui/material/Select';
+import MuiMenuItem from '@mui/material/MenuItem';
+
+export const MuiSelect = styled(MySelect)`
+    height: 56px;
+`;
+
+export const MenuItem = styled(MuiMenuItem)`
+    font-weight: bolder;
+`;
