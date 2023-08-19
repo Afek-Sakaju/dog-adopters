15e6cdc0cdb0503ef9d1490dcc6518312e77ff45
Afek-Sakaju-BetrBeta <a.sakaju_519@contactbetrbeta.onmicrosoft.com>
2023-08-19 14:52:23 +0300

    Removed unused component EditDogForm

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,3 @@
 export { default as RegisterForm } from './RegisterForm/RegisterForm';
 export { default as LoginForm } from './LoginForm/LoginForm';
 export { default as DogForm } from './DogForm/DogForm';
-export { default as EditDogForm } from './EditDogForm/EditDogForm';
