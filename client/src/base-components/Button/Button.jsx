4b98812d122e8596a021efef1c308c4da6aa0b9c
Afek-Sakaju-BetrBeta <a.sakaju_519@contactbetrbeta.onmicrosoft.com>
2023-08-19 15:12:11 +0300

    Added option to add helperText to the Button base cmp

--- client/src/base-components/Button/Button.jsx
+++ client/src/base-components/Button/Button.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { MuiButton } from './Button.styled';
+import { MuiButton, FormHelperText, Box } from './Button.styled';
 
 export default function Button({
     color,
     disabled,
     disableElevation,
     endIcon,
+    error,
     label,
     onClick,
     size,
@@ -17,7 +18,25 @@ export default function Button({
     helperText,
     ...props
 }) {
-    return (
+    return helperText ? (
+        <Box>
+            <MuiButton
+                color={color}
+                disabled={disabled}
+                disableElevation={disableElevation}
+                endIcon={endIcon}
+                onClick={onClick}
+                size={size}
+                startIcon={startIcon}
+                variant={variant}
+                {...props}
+            >
+                {label}
+                {children}
+            </MuiButton>
+            <FormHelperText error={error}>{helperText}</FormHelperText>
+        </Box>
+    ) : (
         <MuiButton
             color={color}
             disabled={disabled}
@@ -40,6 +59,7 @@ Button.propTypes = {
     disabled: PropTypes.bool,
     disableElevation: PropTypes.bool,
     endIcon: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+    error: PropTypes.bool,
     helperText: PropTypes.string,
     label: PropTypes.string,
     onClick: PropTypes.func,
@@ -53,6 +73,7 @@ Button.defaultProps = {
     disabled: undefined,
     disableElevation: undefined,
     endIcon: undefined,
+    error: undefined,
     helperText: undefined,
     label: '',
     onClick: undefined,
