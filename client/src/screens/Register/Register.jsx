d83ea348e2d95cdd7eea685b1f060600947c2e57
Afek-Sakaju-BetrBeta <a.sakaju_519@contactbetrbeta.onmicrosoft.com>
2023-08-22 22:19:08 +0300

    Added some todos comments

--- client/src/screens/Register/Register.jsx
+++ client/src/screens/Register/Register.jsx
@@ -4,42 +4,43 @@ import { RegisterForm } from '@components';
 import { Alert, Snackbar, PageContainer } from './Register.styled';
 
 export default function Register() {
-  const [userData, setUserData] = useState(null);
-  const [responseState, setResponseState] = useState(0);
+    const [userData, setUserData] = useState(null);
+    const [responseState, setResponseState] = useState(0);
 
-  const alert = useMemo(() => {
-    switch (responseState) {
-      case 1:
-        return (
-          <Alert severity="success" variant="filled">
-            Registered successfully
-          </Alert>
-        );
-      case -1:
-        return (
-          <Alert severity="error" variant="filled">
-            Registration failed, try another username.
-          </Alert>
-        );
-      default:
-        return null;
-    }
-  }, [responseState]);
+    // Todo: change alert to be object instead of number
+    const alert = useMemo(() => {
+        switch (responseState) {
+            case 1:
+                return (
+                    <Alert severity="success" variant="filled">
+                        Registered successfully
+                    </Alert>
+                );
+            case -1:
+                return (
+                    <Alert severity="error" variant="filled">
+                        Registration failed, try another username.
+                    </Alert>
+                );
+            default:
+                return null;
+        }
+    }, [responseState]);
 
-  return (
-    <PageContainer>
-      <RegisterForm
-        onSubmit={(data) => setUserData(data)}
-        setResponseState={setResponseState}
-      />
-      <Snackbar
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-        autoHideDuration={6000}
-        onClose={() => setResponseState(0)}
-        open={responseState !== 0}
-      >
-        {alert}
-      </Snackbar>
-    </PageContainer>
-  );
+    return (
+        <PageContainer>
+            <RegisterForm
+                onSubmit={(data) => setUserData(data)}
+                setResponseState={setResponseState}
+            />
+            <Snackbar
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                autoHideDuration={6000}
+                onClose={() => setResponseState(0)}
+                open={responseState !== 0}
+            >
+                {alert}
+            </Snackbar>
+        </PageContainer>
+    );
 }
