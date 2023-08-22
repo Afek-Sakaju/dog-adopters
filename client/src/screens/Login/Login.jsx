bfa45ab0934f244b049ce6b4d188290976cec362
Afek-Sakaju <afeksacajo@email.com>
2023-08-22 22:19:08 +0300

    Added some todos comments

--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -4,42 +4,43 @@ import { LoginForm } from '@components';
 import { Alert, PageContainer, Snackbar } from './Login.styled';
 
 export default function Login() {
-  const [userData, setUserData] = useState(null);
-  const [responseState, setResponseState] = useState(0);
+    const [userData, setUserData] = useState(null);
+    const [responseState, setResponseState] = useState(0);
 
-  const alert = useMemo(() => {
-    switch (responseState) {
-      case 1:
-        return (
-          <Alert severity="success" variant="filled">
-            Logged in successfully
-          </Alert>
-        );
-      case -1:
-        return (
-          <Alert severity="error" variant="filled">
-            Invalid username or password
-          </Alert>
-        );
-      default:
-        return null;
-    }
-  }, [responseState]);
+    // Todo: change alert to be object instead of number
+    const alert = useMemo(() => {
+        switch (responseState) {
+            case 1:
+                return (
+                    <Alert severity="success" variant="filled">
+                        Logged in successfully
+                    </Alert>
+                );
+            case -1:
+                return (
+                    <Alert severity="error" variant="filled">
+                        Invalid username or password
+                    </Alert>
+                );
+            default:
+                return null;
+        }
+    }, [responseState]);
 
-  return (
-    <PageContainer>
-      <LoginForm
-        onSubmit={(data) => setUserData(data)}
-        setResponseState={setResponseState}
-      />
-      <Snackbar
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-        autoHideDuration={6000}
-        onClose={() => setResponseState(0)}
-        open={responseState !== 0}
-      >
-        {alert}
-      </Snackbar>
-    </PageContainer>
-  );
+    return (
+        <PageContainer>
+            <LoginForm
+                onSubmit={(data) => setUserData(data)}
+                setResponseState={setResponseState}
+            />
+            <Snackbar
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                autoHideDuration={6000}
+                onClose={() => setResponseState(0)}
+                open={responseState !== 0}
+            >
+                {alert}
+            </Snackbar>
+        </PageContainer>
+    );
 }
