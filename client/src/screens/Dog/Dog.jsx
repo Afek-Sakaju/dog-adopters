31f6cac02e5fc6fb2ebdd2c31af29124af798767
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-08-22 20:46:44 +0300

    removed console.log

--- client/src/screens/Dog/Dog.jsx
+++ client/src/screens/Dog/Dog.jsx
@@ -124,7 +124,6 @@ export default function CreateDog() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [responseState]);
 
-    console.log(isNew, dogData);
     return (
         <PageContainer>
             {isLoading || errorFetchingDogFromDb ? (
