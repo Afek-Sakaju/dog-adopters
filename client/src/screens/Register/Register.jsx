21c0f419b12c4a6df67c7ecdbd4ead349cbe3feb
Afek-Sakaju <afeksacajo@email.com>
2023-08-23 23:08:17 +0300

    Changed the initialization of the onSubmit in the register form to be initialized at the Register screen instead

--- client/src/screens/Register/Register.jsx
+++ client/src/screens/Register/Register.jsx
@@ -1,11 +1,21 @@
 import React, { useMemo, useState } from 'react';
 
+import { AuthProxy } from '@proxies';
 import { RegisterForm } from '@components';
 import { Alert, Snackbar, PageContainer } from './Register.styled';
 
 export default function Register() {
-    const [userData, setUserData] = useState(null);
     const [responseState, setResponseState] = useState(0);
+
+    const handleSubmit = async (data, onSuccess) => {
+        await AuthProxy.registerUser({ userData: data })
+            .then(() => setResponseState(1))
+            .then(() => onSuccess())
+            .catch((e) => {
+                setResponseState(-1);
+                console.error(e);
+            });
+    };
 
     // Todo: change alert to be object instead of number
     const alert = useMemo(() => {
@@ -29,10 +39,7 @@ export default function Register() {
 
     return (
         <PageContainer>
-            <RegisterForm
-                onSubmit={(data) => setUserData(data)}
-                setResponseState={setResponseState}
-            />
+            <RegisterForm onSubmit={handleSubmit} />
             <Snackbar
                 anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
                 autoHideDuration={6000}
