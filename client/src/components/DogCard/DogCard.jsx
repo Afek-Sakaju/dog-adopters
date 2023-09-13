9f0a156aff434a5fde23f3c1d0dcf05eda0f1267
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-09-14 00:24:24 +0300

    Created Dog Card component's initial implementation

---
+++ client/src/components/DogCard/DogCard.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Card, Text, DesexedIcon, VaccinatedIcon } from './DogCard.styled';
+
+export default function DogCard({
+    age,
+    gender,
+    image,
+    isAdopted,
+    isDesexed,
+    isVaccinated,
+    name,
+    onClick,
+    race,
+    ...props
+}) {
+    return (
+        <Card imageUrl={image} {...props}>
+            {name && <Text>{`I am ${name}`}</Text>}
+            {age && <Text>{`My age is ${age}`}</Text>}
+            {race && <Text>{`And my race is ${race}`}</Text>}
+            {isDesexed && (
+                <>
+                    <DesexedIcon />
+                    <Text>Desexed</Text>
+                </>
+            )}
+            {isVaccinated && (
+                <>
+                    <VaccinatedIcon />
+                    <Text>Vaccinated</Text>
+                </>
+            )}
+            {isAdopted ? (
+                <Text>I'm looking for an adoption</Text>
+            ) : (
+                <Text>I have been adopted</Text>
+            )}
+        </Card>
+    );
+}
+
+DogCard.propTypes = {
+    age: PropTypes.number,
+    gender: PropTypes.string,
+    image: PropTypes.string,
+    isAdopted: PropTypes.bool,
+    isDesexed: PropTypes.bool,
+    isVaccinated: PropTypes.bool,
+    name: PropTypes.string,
+    onClick: PropTypes.func,
+    race: PropTypes.string,
+};
+
+DogCard.defaultProps = {
+    age: undefined,
+    gender: undefined,
+    image: undefined,
+    isAdopted: undefined,
+    isDesexed: undefined,
+    isVaccinated: undefined,
+    name: undefined,
+    onClick: undefined,
+    race: undefined,
+};
---
+++ client/src/components/DogCard/DogCard.styled.jsx
@@ -0,0 +1,27 @@
+import { styled } from '@mui/material/styles';
+import MuiTypography from '@mui/material/Typography';
+import { FaShieldDog } from 'react-icons/fa6';
+import { TbVaccine } from 'react-icons/tb';
+
+import { Card as MyCard } from '@base-components';
+
+export const Card = MyCard;
+
+export const Text = styled(MuiTypography)`
+    font-size: 1em;
+    margin: 0;
+`;
+
+export const DesexedIcon = styled(FaShieldDog)`
+    padding: 3px;
+    width: 25px;
+    height: 25px;
+    color: #388e3c;
+`;
+
+export const VaccinatedIcon = styled(TbVaccine)`
+    padding: 3px;
+    width: 25px;
+    height: 25px;
+    color: #388e3c;
+`;
