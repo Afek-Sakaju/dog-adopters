0fa1e4d49e00c38c2e6bf39b6f3d4eb6f24f0cd9
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-11-03 12:31:39 +0200

    renamed prop-types file name to mui-props

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,4 @@
 export * from './functions';
 export * from './constants/content-constants';
 export * from './constants/data-constants';
-export * from './constants/prop-types-constants';
+export * from './constants/mui-props-constants';
