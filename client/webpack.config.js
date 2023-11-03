75a08ec883ec67e08a02b3e692191b30638caa57
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-11-03 08:23:31 +0200

    Renamed Screens folder to Pages for better naming convention

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
             '@components': path.join(__dirname, 'src/components/'),
             '@base-components': path.join(__dirname, 'src/base-components/'),
             '@utils': path.join(__dirname, 'src/utils/'),
-            '@screens': path.join(__dirname, 'src/screens/'),
+            '@pages': path.join(__dirname, 'src/pages/'),
             '@store': path.join(__dirname, 'src/store/'),
             '@validations': path.join(__dirname, 'src/validations/'),
             '@proxies': path.join(__dirname, 'src/proxies/'),
