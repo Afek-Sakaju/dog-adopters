fb1e34cff3121d9d860ecace992a7a7dfbaa9c18
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-11-10 17:12:52 +0200

    improved scrollbar UI

--- client/src/components/DogsDataList/DogsDataList.styled.jsx
+++ client/src/components/DogsDataList/DogsDataList.styled.jsx
@@ -14,6 +14,26 @@ export const DogsListContainer = styled(MuiPaper)`
     gap: 5px;
     padding: 15px;
     overflow-y: auto;
+    overflow-x: hidden;
+
+    transform: translateZ(0);
+    -webkit-transform: translateZ(0);
+
+    &::-webkit-scrollbar {
+        width: 12px;
+    }
+
+    &::-webkit-scrollbar-track {
+        border-radius:  0 3px 3px 0;
+        background-color: #e7e7e7;
+        border: 1px solid #dadada9a
+        box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+    }
+
+    &::-webkit-scrollbar-thumb {
+        border-radius:  0 3px 3px 0;
+        background-color: #727272;
+    }
 `;
 
 export const DogCard = styled(MyDogCard)``;
