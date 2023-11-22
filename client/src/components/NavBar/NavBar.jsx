1b8ab109e7d21dd6c81d26c06b87820bc1298020
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-11-22 18:42:11 +0200

    Added logic to the navbar for when the user is logged in to show just a logout button and avatar of the user

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,26 +1,68 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { connect } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
-import { LoginButton, RegisterButton, AppBar, NavLogo } from './NavBar.styled';
+import { MUI_VARIANTS } from '@utils';
+import { getUserReselectSelector, removeUserAction } from '@store';
+import { AppBar, Avatar, Button, NavLogo } from './NavBar.styled';
 
-export default function NavBar({ children, ...props }) {
+const NavBar = ({ children, user, onLogout, ...props }) => {
     const handleSubmit = () => {};
+
+    const location = useLocation();
+
+    const isLoggedIn = !!user?._id;
+
+    const isLoginPage = location?.pathname === '/login';
+    const isRegisterPage = location?.pathname === '/register';
 
     return (
         <AppBar
             startCmp={<NavLogo alt="/logo" src="/nav-logo.png" />}
             {...props}
         >
-            <LoginButton
-                label="Log-In"
-                fullWidth
-                onClick={() => handleSubmit()}
-            />
-            <RegisterButton
-                label="Register"
-                fullWidth
-                onClick={() => handleSubmit()}
+            {isLoggedIn ? (
+                <Button
+                    fullWidth
+                    invertColors
+                    label="Log-Out"
+                    onClick={() => handleSubmit()}
+                />
+            ) : (
+                <>
+                    <Button
+                        fullWidth
+                        invertColors
+                        label="Log-In"
+                        onClick={() => handleSubmit()}
+                        isSelected={isLoginPage}
+                    />
+                    <Button
+                        label="Register"
+                        fullWidth
+                        onClick={() => handleSubmit()}
+                        isSelected={isRegisterPage}
+                    />
+                </>
+            )}
+            <Avatar
+                username={user?.username}
+                variant={MUI_VARIANTS.AVATAR.VALUES.CIRCULAR}
             />
             {children}
         </AppBar>
     );
-}
+};
+
+const mapStateToProps = (state) => ({
+    user: getUserReselectSelector(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    onLogout: (user) => {
+        dispatch(removeUserAction({ user }));
+    },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
--- client/src/components/NavBar/NavBar.styled.jsx
+++ client/src/components/NavBar/NavBar.styled.jsx
@@ -1,37 +1,53 @@
 import { styled } from '@mui/material/styles';
 
-import { Button as MyButton, AppBar as MyAppBar } from '@base-components';
+import {
+    Button as MyButton,
+    AppBar as MyAppBar,
+    Avatar as MyAvatar,
+} from '@base-components';
 
-export const LoginButton = styled(MyButton)`
-    padding: 4px 0;
-    color: #1976d2;
-    outline: #1976d2 1px solid;
-    border-radius: 10px;
-    background-color: white;
-    font-size: 1em;
-    font-weight: bold;
-    text-transform: none;
+export const Button = styled(MyButton, {
+    shouldForwardProp: (prop) =>
+        prop !== 'invertColors' && prop !== 'isSelected',
+})(({ invertColors, isSelected }) => ({
+    width: '100px',
+    padding: '4px 0',
+    borderRadius: '10px',
+    fontSize: '1em',
+    fontWeight: 'bold',
+    textTransform: 'none',
+    marginLeft: 'auto',
 
-    &:hover {
-        background-color: #81b8f03a;
-    }
-`;
+    ...(invertColors ? { color: '#1976d2' } : { color: 'white' }),
+    ...(invertColors && { backgroundColor: 'white' }),
+    ...(invertColors
+        ? { outline: '#1976d2 1px solid' }
+        : { outline: '#0959a9 1px solid' }),
 
-export const RegisterButton = styled(MyButton)`
-    padding: 4px 0;
-    border-radius: 10px;
-    font-size: 1em;
-    font-weight: bold;
-    text-transform: none;
+    '&:hover': {
+        ...(invertColors
+            ? { backgroundColor: '#81b8f03a' }
+            : { backgroundColor: '#185ca0' }),
+    },
 
-    &:hover {
-        background-color: #185ca0;
-    }
-`;
+    ...(isSelected && {
+        '&::after': {
+            content: '""',
+            position: 'absolute',
+            bottom: '-17px',
+            left: '0',
+            width: '100%',
+            height: '3px',
+            backgroundColor: '#185ca0',
+        },
+    }),
+}));
 
 export const AppBar = styled(MyAppBar)`
     background-color: white;
 `;
+
+export const Avatar = styled(MyAvatar)``;
 
 export const NavLogo = styled('img')`
     width: 124px;
