ffa307d405a5773df8b014655141fe183712d707
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2024-04-06 00:10:02 +0300

    fixed a bug on the server side where it was crashing for no reason due to incorrect usage of hostname url (localhost instead of 127.0.0.1)
    and also improved some variables re use and code readability

--- server/src/utils/environment-variables.ts
+++ server/src/utils/environment-variables.ts
@@ -7,6 +7,7 @@ expand(config({ path: DOT_ENV_PATH }));
 
 export const PORT: number = +(process.env.PORT ?? 3005);
 export const MONGO_URL: string = process.env.MONGO_URL ?? '';
+export const MONGO_HOST: string = process.env.MONGO_HOST ?? '';
 export const CORS_ORIGIN_URL: string = process.env.CORS_ORIGIN_URL ?? '';
 export const NODE_ENV: string = process.env.NODE_ENV ?? 'local';
 export const LOGGING_MODE: string = process.env.LOGGING_MODE ?? 'error';
