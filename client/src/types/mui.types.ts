77586a6e30511c8773f88c9f0ac40148a95e6ca4
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2024-04-20 16:04:48 +0300

    Modified the comments of some types to be more readable

--- client/src/types/mui.types.ts
+++ client/src/types/mui.types.ts
@@ -6,13 +6,13 @@ import {
     MUI_INPUT_VARIANTS,
 } from '../utils/constants/mui-props.constants';
 
-// "error" | "info" | "primary" | "success" | "warning"
+// ~Color:  "error" | "info" | "primary" | "success" | "warning"
 export type MuiColor = (typeof MUI_COLORS_LIST)[number];
 
-// "number" | "text" | "password" | "email" | "search"
+// ~Input Type:  "number" | "text" | "password" | "email" | "search"
 export type MuiInputType = (typeof MUI_INPUT_TYPES_LIST)[number];
 
-// "outlined" | "filled" | "standard"
+// ~Input Variant: "outlined" | "filled" | "standard"
 export type MuiInputVariant = (typeof MUI_INPUT_VARIANTS)[number];
 
 export type MuiStyledCmpThemeProp = { theme: Theme };
