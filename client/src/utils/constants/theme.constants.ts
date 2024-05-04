94e022187ca55330904b884fb9cfd21a49b834e4
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2024-05-04 18:05:04 +0300

    refactor: added more usage of colors constants

--- client/src/utils/constants/theme.constants.ts
+++ client/src/utils/constants/theme.constants.ts
@@ -4,6 +4,16 @@ export const MAIN_COLORS = {
     primary: '#F1AB61',
     primaryLight: '#ffdda6a6',
     secondary: '#b1a195',
+    bgColor: 'white',
+    bgAltColor: '#f2f2f2',
+    error: '#d32f2f',
+    text: 'black',
+    textSecondary: '#735A47',
+    textSecondaryLight: '#FEE9C5',
+    male: '#2986cc',
+    female: '#c90076',
+    safe: '#388e3c',
+    feature: '#09298',
 } as const;
 
 export const theme = createTheme({
