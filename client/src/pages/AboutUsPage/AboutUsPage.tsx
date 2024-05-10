d40ded90b112a1de830ac35cc1a853c672506c56
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2024-05-10 10:08:13 +0300

    added the layout, design, and content of the AboutUsPage
    fixed the alert message content after the login process

--- client/src/pages/AboutUsPage/AboutUsPage.tsx
+++ client/src/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,5 +1,41 @@
 import React, { type ReactNode } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import {
+    MainContentWrapper,
+    PageContainer,
+    Text,
+    TextSectionContainer,
+    Title,
+} from './AboutUsPage.styled';
 
 export default function AboutUsPage(): ReactNode {
-    return <div>AboutUsPage</div>;
+    const { t } = useTranslation();
+
+    return (
+        <PageContainer>
+            <MainContentWrapper>
+                <TextSectionContainer>
+                    <Title>{t('about_us.who_are_we.title')}</Title>
+                    <Text>{t('about_us.who_are_we.content')}</Text>
+                </TextSectionContainer>
+                <TextSectionContainer>
+                    <Title>{t('about_us.our_mission.title')}</Title>
+                    <Text>{t('about_us.our_mission.content')}</Text>
+                </TextSectionContainer>
+                <TextSectionContainer>
+                    <Title>{t('about_us.future_plans.title')}</Title>
+                    <Text>{t('about_us.future_plans.content')}</Text>
+                </TextSectionContainer>
+                <TextSectionContainer>
+                    <Title>{t('about_us.key_features.title')}</Title>
+                    <Text>{t('about_us.key_features.content')}</Text>
+                </TextSectionContainer>
+                <TextSectionContainer>
+                    <Title>{t('about_us.the_creator.title')}</Title>
+                    <Text>{t('about_us.the_creator.content')}</Text>
+                </TextSectionContainer>
+            </MainContentWrapper>
+        </PageContainer>
+    );
 }
