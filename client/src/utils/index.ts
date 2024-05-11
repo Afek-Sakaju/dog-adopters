57170dd55ae94d899b17dcba6c0e78342e9a71fe
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2024-05-11 23:23:55 +0300

    renamed the file common-styles to styles

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,4 +1,4 @@
-export * from './constants/common-styles.constants';
+export * from './constants/style.constants';
 export * from './constants/content.constants';
 export * from './constants/data.constants';
 export * from './constants/mui-props.constants';
