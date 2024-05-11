687110a5d3cf7a02cf30002cf8fea199a513d0ac
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2024-05-11 14:05:30 +0300

    added better usage of  the mainScrollbarStyles as a variable, which will be used app wide later

--- client/src/pages/pages.styled.tsx
+++ client/src/pages/pages.styled.tsx
@@ -1,10 +1,30 @@
-import { styled } from '@mui/material/styles';
+import { styled, css } from '@mui/material/styles';
 
 import { Box as MuiBox } from '@mui/material';
 
-// eslint-disable-next-line import/prefer-default-export
 export const LayoutWrapper = styled(MuiBox)`
     height: 100vh;
     display: flex;
     flex-direction: column;
+`;
+
+export const mainScrollbarStyles = css`
+    ::-webkit-scrollbar {
+        width: 20px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background-color: transparent;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: #dddddd;
+        border-radius: 15px;
+        border: 6px solid transparent;
+        background-clip: content-box;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background-color: #a7a7a7;
+    }
 `;
