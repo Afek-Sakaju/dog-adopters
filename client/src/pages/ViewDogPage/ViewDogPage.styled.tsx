b0ccd84e364b3199194b678237ab906092737d51
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2024-05-18 16:25:54 +0300

    improved the style layout and ui of the DogViewPage

--- client/src/pages/ViewDogPage/ViewDogPage.styled.tsx
+++ client/src/pages/ViewDogPage/ViewDogPage.styled.tsx
@@ -7,28 +7,53 @@ import {
 } from '@mui/material';
 import { MdOutlineModeEditOutline as PencilIcon } from 'react-icons/md';
 
-import { Loader as MyLoader, Card as MyCard } from '@/base-components';
+import { Loader as MyLoader } from '@/base-components';
 import { ClearIconButton as MyIconButton } from '@/components';
+import { MAIN_COLORS } from '@/utils';
 
 export const PageContainer = styled('div')`
+    height: 100%;
+    width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100%;
-    width: 100%;
+    margin-bottom: 40px;
 `;
 
-export const MainContainer = styled(MuiBox)``;
+export const MainContainer = styled(MuiBox)`
+    width: 60%;
+    display: flex;
+    border: 1px solid ${MAIN_COLORS.bgAltColor};
+`;
 
-export const DogImagePreviewContainer = styled(MuiBox)``;
+export const DogImagePreviewContainer = styled(MuiBox)`
+    flex: 5;
+`;
 
-export const EditDogSection = styled(MuiBox)``;
+export const EditDogSection = styled(MuiBox)`
+    display: flex;
+    justify-content: center;
+`;
 
-export const DogInformationContentWrapper = styled(MuiBox)``;
+export const DogInformationContentWrapper = styled(MuiBox)`
+    flex: 4;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    padding: 10px 20px;
+`;
 
-export const DogImage = styled(MyCard)``;
+export const DogImage = styled('img')`
+    height: auto;
+    width: 100%;
+    aspect-ratio: 5/4;
+    object-fit: cover;
+    object-position: center;
+`;
 
-export const Text = styled(MuiTypography)``;
+export const Text = styled(MuiTypography)`
+    font-size: 1.1rem;
+`;
 
 export const Snackbar = MuiSnackbar;
 
@@ -40,4 +65,7 @@ export const Loader = styled(MyLoader)`
 
 export const IconButton = styled(MyIconButton)``;
 
-export const EditIcon = styled(PencilIcon)``;
+export const EditIcon = styled(PencilIcon)`
+    font-size: 1.1rem;
+    color: ${MAIN_COLORS.safe};
+`;
